b4c47dd01ce991642358d8e62f93e1681998170c
annav1asova <annasv209@gmail.com>
2021-04-19 13:46:00 +0300

    Remove unnecessary task

--- kotlin-analysis-core/build.gradle.kts
+++ kotlin-analysis-core/build.gradle.kts
@@ -1,15 +1,2 @@
 group = rootProject.group
 version = rootProject.version
-
-tasks.register<Jar>("testJar") {
-    archiveClassifier.set("tests")
-    from(sourceSets.test.get().output)
-}
-
-configurations.create("tests") {
-    extendsFrom(configurations.testRuntime.get())
-}
-
-artifacts {
-    add("tests", tasks.named("testJar"))
-}
