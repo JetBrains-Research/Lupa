0fcc42f78dac68cdcf0fbbb100be02aa087463fb
Anna <annasv209@gmail.com>
2021-04-29 11:29:11 +0300

    Close projects

--- kotlin-analysis-clones/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/FormattedMethodMiner.kt
+++ kotlin-analysis-clones/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/FormattedMethodMiner.kt
@@ -6,6 +6,8 @@ import org.jetbrains.research.ml.kotlinAnalysis.psi.PsiProvider
 import org.jetbrains.research.ml.kotlinAnalysis.util.getPrintWriter
 import org.jetbrains.research.ml.kotlinAnalysis.util.getSubdirectories
 import java.nio.file.Path
+import com.intellij.openapi.project.ex.ProjectManagerEx
+
 
 /**
  * Extracts all methods from projects in the dataset and saves methods in Clone Detection Tool
@@ -16,23 +18,34 @@ class FormattedMethodMiner(outputDir: Path) {
     private val methodDataWriter = getPrintWriter(outputDir, "method_data.txt")
 
     fun extractMethodsToCloneDetectionFormat(inputDir: Path) {
-        getSubdirectories(inputDir).forEach { projectPath ->
-            ApplicationManager.getApplication().runReadAction {
-                ProjectUtil.openOrImport(projectPath, null, true).let { project ->
-                    val projectIndex = indexer.indexProject(project)
-                    println("Start processing project ${project.name} (index $projectIndex)")
-                    val methods = PsiProvider.extractMethodsFromProject(project)
-                    val methodsIndexed = methods.associateWith { method ->
-                        indexer.indexMethod(method, projectIndex)
-                    }
-                    methodsIndexed.forEach { (method, methodIndex) ->
-                        val methodFormatted = CloneDetectionAdapter.format(method, projectIndex, methodIndex)
-                        methodDataWriter.println(methodFormatted)
+        try {
+            getSubdirectories(inputDir).forEach { projectPath ->
+                ApplicationManager.getApplication().runReadAction {
+                    ProjectUtil.openOrImport(projectPath, null, true).let { project ->
+                        try {
+                            val projectIndex = indexer.indexProject(project)
+                            println("Start processing project ${project.name} (index $projectIndex)")
+                            val methods = PsiProvider.extractMethodsFromProject(project)
+                            val methodsIndexed = methods.associateWith { method ->
+                                indexer.indexMethod(method, projectIndex)
+                            }
+                            methodsIndexed.forEach { (method, methodIndex) ->
+                                val methodFormatted = CloneDetectionAdapter.format(method, projectIndex, methodIndex)
+                                methodDataWriter.println(methodFormatted)
+                            }
+                        } catch (ex: Exception) {
+                            println(ex.message)
+                        } finally {
+                            ApplicationManager.getApplication().invokeAndWait {
+                                ProjectManagerEx.getInstanceEx().closeAndDispose(project)
+                            }
+                        }
                     }
                 }
             }
+        } finally {
+            close()
         }
-        close()
     }
 
     private fun close() {
