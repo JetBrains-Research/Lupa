008bd17272953914920fa49a02f5ae8dec454704
Nastya Birillo <anastasia.i.birillo@gmail.com>
2021-05-06 12:40:01 +0300

    Fix comments removal (#7)

--- kotlin-analysis-clones/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/psi/PsiProvider.kt
+++ kotlin-analysis-clones/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/psi/PsiProvider.kt
@@ -4,10 +4,7 @@ import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.VfsUtilCore
-import com.intellij.psi.PsiComment
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiManager
+import com.intellij.psi.*
 import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.research.ml.kotlinAnalysis.util.isKotlinRelatedFile
@@ -44,12 +41,17 @@ object PsiProvider {
 
     fun deleteComments(element: PsiElement) {
         val comments = PsiTreeUtil.collectElementsOfType(element, PsiComment::class.java)
-        // If we do not change the order of the elements, then a parent can invalidate
-        // the child element, but it can also be a comment and an exception will be thrown,
-        // so we must delete the found comments in the reverse order
-        comments.reversed().forEach {
-            WriteCommandAction.runWriteCommandAction(it.project) {
-                it.delete()
+        // We should handle each group of elements with same parent separately
+        // since if we delete the first item in this group we invalidate
+        // other elements with the same parent
+        comments.toList().groupBy { it.parent }.entries.forEach {
+            // If we do not change the order of the elements, then a parent can invalidate
+            // the child element, but it can also be a comment and an exception will be thrown,
+            // so we must delete the found comments in the reverse order
+            it.value.reversed().forEach { comment ->
+                WriteCommandAction.runWriteCommandAction(comment.project) {
+                    comment.delete()
+                }
             }
         }
     }
---
+++ kotlin-analysis-clones/src/test/resources/org/jetbrains/research/ml/kotlinAnalysis/psi/data/out_9.kt
@@ -0,0 +1,246 @@
+package com.swensun.func.recycler
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.*
+import com.drakeet.multitype.ItemViewDelegate
+import com.drakeet.multitype.MultiTypeAdapter
+import com.swensun.potato.R
+import kotlinx.android.synthetic.main.item_recycler_view.view.*
+import kotlinx.android.synthetic.main.load_more.view.*
+import kotlinx.android.synthetic.main.recycler_view_fragment.*
+
+
+class RecyclerViewFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RecyclerViewFragment()
+    }
+
+    private lateinit var viewModel: RecyclerViewViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.recycler_view_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(RecyclerViewViewModel::class.java)
+        initView()
+    }
+
+    private fun initView() {
+        recycler_view.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(context)
+        recycler_view.layoutManager = layoutManager
+
+
+        val adapter = DiffMultiAdapter()
+        recycler_view.adapter = adapter
+        adapter.register(RViewHolderDelegate().apply {
+            setCountListener { position, count ->
+                adapterItems[position].let {
+                    if (it is RInt) {
+                        it.count = count + 1
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        })
+        adapter.register(LoadMoreDelegate {
+            val items = arrayListOf<Any>().apply {
+                addAll(adapter.items)
+            }
+            if (items.size < 50) {
+                items.addAll((items.size until items.size + 10).map { RInt(it) })
+                items.add(LoadMore())
+            } else {
+                items.add(LoadMore(false))
+            }
+            adapter.items = items
+            recycler_view.post {
+                adapter.notifyDataSetChanged()
+            }
+        })
+
+        adapter.submitList((0 until 10).map { RInt(it) })
+
+        btn_refresh.setOnClickListener {
+            val items = adapter.items.toMutableList()
+            items.add(0, RInt(items.size))
+            adapter.items = items
+            adapter.notifyItemInserted(0)
+        }
+    }
+}
+
+
+class RAdapter : RecyclerView.Adapter<RViewHolder>() {
+
+    var currentList: ArrayList<RInt> = arrayListOf()
+        set(value) {
+            field.clear()
+            field.addAll(value)
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RViewHolder {
+        return RViewHolder(parent)
+    }
+
+    override fun getItemCount(): Int {
+        return currentList.size
+    }
+
+    override fun onBindViewHolder(holder: RViewHolder, position: Int) {
+        holder.setup(currentList[position], countListener)
+    }
+
+
+    private var countListener: ((position: Int, count: Int) -> Unit)? = null
+    fun setCountListener(function: (position: Int, count: Int) -> Unit) {
+        this.countListener = function
+    }
+
+    fun submitList(list: List<RInt>) {
+        currentList.clear()
+        currentList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+}
+
+class RViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_view, parent, false)
+) {
+
+    fun setup(
+        item: RInt,
+        countListener: ((position: Int, count: Int) -> Unit)?
+    ) {
+        itemView.tv_id.text = "${item.count}"
+        itemView.setOnClickListener {
+            countListener?.invoke(adapterPosition, item.count)
+        }
+    }
+}
+
+class RInt(val id: Int) {
+    var count = id
+
+    fun clone(): RInt {
+        val rint = RInt(id)
+        rint.count = count
+        return rint
+    }
+}
+
+class RViewHolderDelegate : ItemViewDelegate<RInt, RViewHolder>() {
+
+    private var countListener: ((position: Int, count: Int) -> Unit)? = null
+    fun setCountListener(function: (position: Int, count: Int) -> Unit) {
+        this.countListener = function
+    }
+
+    override fun onBindViewHolder(holder: RViewHolder, item: RInt) {
+        holder.setup(item, countListener)
+    }
+
+    override fun onCreateViewHolder(context: Context, parent: ViewGroup): RViewHolder {
+        return RViewHolder(parent)
+    }
+
+}
+
+class LoadMoreDelegate(private val block: () -> Unit) :
+    ItemViewDelegate<LoadMore, LoadMoreDelegate.LoadMoreViewHolder>() {
+
+    inner class LoadMoreViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.load_more, parent, false)
+    ) {
+        fun setup(item: LoadMore) {
+            if (item.hasMore) {
+                itemView.tv_more.text = "....."
+            } else {
+                itemView.tv_more.text = "到底了"
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(context: Context, parent: ViewGroup): LoadMoreViewHolder {
+        return LoadMoreViewHolder(parent)
+    }
+
+    override fun onBindViewHolder(holder: LoadMoreViewHolder, item: LoadMore) {
+        holder.setup(item)
+        if (item.hasMore) {
+            block.invoke()
+        }
+    }
+}
+
+class LoadMore(val hasMore: Boolean = true) {
+    var type = 0
+}
+
+class RIntCallback(val oldItems: List<Any>, val newItems: List<Any>) : DiffUtil.Callback() {
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val old = oldItems[oldItemPosition]
+        val new = newItems[newItemPosition]
+        when {
+            old is RInt && new is RInt -> {
+                return old.id == new.id
+            }
+        }
+        return false
+    }
+
+    override fun getOldListSize(): Int {
+        return oldItems.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newItems.size
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val old = oldItems[oldItemPosition]
+        val new = newItems[newItemPosition]
+        when {
+            old is RInt && new is RInt -> {
+                return old.count == new.count
+            }
+        }
+        return false
+    }
+
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+        return newItems[newItemPosition]
+    }
+}
+
+class DiffMultiAdapter : MultiTypeAdapter() {
+
+    fun submitList(newItems: List<Any>) {
+        val callback = RIntCallback(items, newItems)
+        val result = DiffUtil.calculateDiff(callback)
+        items = newItems
+        result.dispatchUpdatesTo(this)
+    }
+}
+
+fun RecyclerView.closeDefaultAnimator() {
+    if (itemAnimator is SimpleItemAnimator) {
+        (this.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
+    }
+}
+
