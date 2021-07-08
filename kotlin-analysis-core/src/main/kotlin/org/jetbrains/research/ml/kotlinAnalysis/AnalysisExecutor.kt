a13ecbeb944b0d9b2cea4e299682e658e8673a0b
tiginamaria <tiginamaria@gmail.com>
2021-07-08 20:44:31 +0300

    Improve architecture. Add recourse controlling to executor and unify runner classes

--- kotlin-analysis-core/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/AnalysisExecutor.kt
+++ kotlin-analysis-core/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/AnalysisExecutor.kt
@@ -2,10 +2,14 @@ package org.jetbrains.research.ml.kotlinAnalysis
 
 import com.intellij.ide.impl.ProjectUtil
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ex.ProjectManagerEx
 import org.jetbrains.research.ml.kotlinAnalysis.util.getSubdirectories
 import java.nio.file.Path
+
+/** Setup for common IDEA project. For given [project path][projectPath] creates an instance of [project][Project]. */
+val openOrImportSetup: (Path) -> Project = { projectPath -> ProjectUtil.openOrImport(projectPath, null, true) }
 
 /**
  * Abstract class for analysis executor which provides interface for execution analysis
@@ -13,26 +17,39 @@ import java.nio.file.Path
  */
 abstract class AnalysisExecutor {
 
+    private val logger: Logger = Logger.getInstance(javaClass)
+
+    /**
+     * Set of resources which are under control of executor. Executor[AnalysisExecutor] runs their initialization
+     * before analysis and close them after it ends.
+     */
+    abstract val controlledResources: Set<Resource>
+
     /** Executes the analysis of the given [project][Project]. */
     abstract fun analyse(project: Project)
 
-    /**
-     * Finishes the task execution. Should contain close functions of all writers and readers
-     * and called after all project analysis execution finished.
-     */
-    abstract fun finish()
+    /** Runs before analysis execution process. Contains all controlled recourses initialization. */
+    private fun start() {
+        controlledResources.forEach { it.init() }
+    }
+
+    /** Runs after analysis execution process. Closes all controlled recourses. */
+    private fun finish() {
+        controlledResources.forEach { it.close() }
+    }
 
     /** Execute analysis for all projects in [given directory][projectsDir]. */
-    fun execute(projectsDir: Path) {
+    fun execute(setupProject: (Path) -> Project, projectsDir: Path) {
+        start()
         try {
             getSubdirectories(projectsDir).forEach { projectPath ->
-                ApplicationManager.getApplication().runReadAction {
+                ApplicationManager.getApplication().invokeAndWait {
                     println("Opening project $projectPath")
-                    ProjectUtil.openOrImport(projectPath, null, true).let { project ->
+                    setupProject(projectPath).let { project ->
                         try {
                             analyse(project)
                         } catch (ex: Exception) {
-                            println(ex.message)
+                            logger.error(ex)
                         } finally {
                             ApplicationManager.getApplication().invokeAndWait {
                                 ProjectManagerEx.getInstanceEx().forceCloseProject(project)
--- kotlin-analysis-dependencies/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/ImportDirectivesAnalysisExecutor.kt
+++ kotlin-analysis-dependencies/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/ImportDirectivesAnalysisExecutor.kt
@@ -2,7 +2,6 @@ package org.jetbrains.research.ml.kotlinAnalysis
 
 import com.intellij.openapi.project.Project
 import org.jetbrains.research.ml.kotlinAnalysis.psi.PsiProvider
-import org.jetbrains.research.ml.kotlinAnalysis.util.getPrintWriter
 import java.nio.file.Path
 
 /**
@@ -11,15 +10,12 @@ import java.nio.file.Path
  */
 class ImportDirectivesAnalysisExecutor(outputDir: Path) : AnalysisExecutor() {
 
-    private val dependenciesDataWriter = getPrintWriter(outputDir, "import_directives_data.txt")
+    private val dependenciesDataWriter = PrintWriterRecourse(outputDir, "import_directives_data.csv")
+    override val controlledResources: Set<Resource> = setOf(dependenciesDataWriter)
 
     override fun analyse(project: Project) {
         val importDirectives = PsiProvider.extractImportDirectiveFromProject(project)
         val results = importDirectives.map { ImportDirectivePsiAnalyzer.analyze(it) }
-        dependenciesDataWriter.println(results.joinToString(separator = "\n"))
-    }
-
-    override fun finish() {
-        dependenciesDataWriter.close()
+        dependenciesDataWriter.writer.println(results.joinToString(separator = "\n"))
     }
 }
