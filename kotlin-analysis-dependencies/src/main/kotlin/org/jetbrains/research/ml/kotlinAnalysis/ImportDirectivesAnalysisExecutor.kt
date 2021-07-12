b1cd9ddd327cdc1b252bef8b848ae7844696d1fb
tiginamaria <tiginamaria@gmail.com>
2021-07-12 15:15:32 +0300

    Actualize tests

--- kotlin-analysis-dependencies/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/ImportDirectivesAnalysisExecutor.kt
+++ kotlin-analysis-dependencies/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/ImportDirectivesAnalysisExecutor.kt
@@ -2,6 +2,7 @@ package org.jetbrains.research.ml.kotlinAnalysis
 
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.psi.KtImportDirective
+import org.jetbrains.kotlin.psi.KtPackageDirective
 import org.jetbrains.research.ml.kotlinAnalysis.psi.PsiProvider
 import java.nio.file.Path
 
@@ -16,8 +17,13 @@ class ImportDirectivesAnalysisExecutor(outputDir: Path, filename: String = "impo
     override val controlledResourceManagers: Set<ResourceManager> = setOf(dependenciesDataWriter)
 
     override fun analyse(project: Project) {
+        val packageDirectives = PsiProvider.extractElementsOfTypeFromProject(project, KtPackageDirective::class.java)
+            .filter { !it.isRoot }
+        val projectPackageFqNames = packageDirectives.map { PackageDirectivePsiAnalyzer.analyze(it) }.toSet()
         val importDirectives = PsiProvider.extractElementsOfTypeFromProject(project, KtImportDirective::class.java)
-        val results = importDirectives.map { ImportDirectivePsiAnalyzer.analyze(it) }
+        val results = importDirectives
+            .map { ImportDirectivePsiAnalyzer.analyze(it) }
+            .filter { importDirective -> !projectPackageFqNames.any { importDirective.startsWith(it) } }
         if (results.isNotEmpty()) {
             dependenciesDataWriter.writer.println(results.joinToString(separator = "\n"))
         }
