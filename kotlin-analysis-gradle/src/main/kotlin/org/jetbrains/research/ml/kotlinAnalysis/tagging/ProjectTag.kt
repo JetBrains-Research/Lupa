2af4e239341d93b8eec4e644b3e5a73f32ab9ab8
tiginamaria <tiginamaria@gmail.com>
2021-07-29 21:45:18 +0300

    Add comments

--- kotlin-analysis-gradle/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/tagging/ProjectTag.kt
+++ kotlin-analysis-gradle/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/tagging/ProjectTag.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.research.ml.kotlinAnalysis.tagging
 
+/** Tag for project which describes it's content of theme. */
 enum class ProjectTag(val value: String?) {
     UNDEFINED("undefined"),
     ANDROID("android"),
