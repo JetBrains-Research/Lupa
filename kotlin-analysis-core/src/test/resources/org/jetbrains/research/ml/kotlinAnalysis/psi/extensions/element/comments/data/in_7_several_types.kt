86fb5e7e6c91287d439f687faf6b77513fe6194a
tiginamaria <tiginamaria@gmail.com>
2021-07-29 17:25:58 +0300

    Fix gradle analysis implementation tests

---
+++ kotlin-analysis-core/src/test/resources/org/jetbrains/research/ml/kotlinAnalysis/psi/extensions/element/comments/data/in_7_several_types.kt
@@ -0,0 +1,20 @@
+import java.util.*
+
+/**
+ * Some KDoc
+ */
+//Some comment
+//Some comment
+//    Some comment
+//Some comment
+//
+//Some comment
+//    Some comment
+//Some comment
+
+fun main(args: Array<String>) {
+    val list = ArrayList<Any>()
+    list.add("Hello")
+    list.add(0)
+    list.map(::println)
+}
