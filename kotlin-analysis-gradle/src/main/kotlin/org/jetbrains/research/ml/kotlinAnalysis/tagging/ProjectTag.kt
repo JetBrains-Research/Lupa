fba9d3384c5961734526b2d44dae6d046c47a52b
tiginamaria <tiginamaria@gmail.com>
2021-07-30 11:12:06 +0300

    Update readme

--- kotlin-analysis-gradle/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/tagging/ProjectTag.kt
+++ kotlin-analysis-gradle/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/tagging/ProjectTag.kt
@@ -2,7 +2,6 @@ package org.jetbrains.research.ml.kotlinAnalysis.tagging
 
 /** Tag for project which describes it's content of theme. */
 enum class ProjectTag(val value: String?) {
-    UNDEFINED("undefined"),
     ANDROID("android"),
     OTHER("other");
 }
--- kotlin-analysis-gradle/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/tagging/ProjectsTaggingExecutor.kt
+++ kotlin-analysis-gradle/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/tagging/ProjectsTaggingExecutor.kt
@@ -1,11 +1,9 @@
 package org.jetbrains.research.ml.kotlinAnalysis.tagging
 
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import org.jetbrains.research.ml.kotlinAnalysis.AnalysisExecutor
 import org.jetbrains.research.ml.kotlinAnalysis.PrintWriterResourceManager
 import org.jetbrains.research.ml.kotlinAnalysis.ResourceManager
-import org.jetbrains.research.ml.kotlinAnalysis.gradle.GradleFileManager
 import java.nio.file.Path
 
 /**
@@ -15,14 +13,6 @@ import java.nio.file.Path
 class ProjectsTaggingExecutor(outputDir: Path, filename: String = "not_android_projects_data.csv") :
     AnalysisExecutor() {
 
-    private val logger: Logger = Logger.getInstance(javaClass)
-
-    companion object {
-
-        /** Library name which indicates that the project is android. */
-        const val ANDROID_DEPENDENCY_GROUP_NAME = "com.android.tools.build"
-    }
-
     private val projectsDataWriter = PrintWriterResourceManager(
         outputDir, filename,
         listOf("full_name", "tag").joinToString(separator = ",")
@@ -30,32 +20,17 @@ class ProjectsTaggingExecutor(outputDir: Path, filename: String = "not_android_p
 
     override val controlledResourceManagers: Set<ResourceManager> = setOf(projectsDataWriter)
 
+    private val taggers: List<ProjectTagger> = listOf(AndroidProjectTagger)
+
     override fun analyse(project: Project) {
+        val projectTags = taggers.flatMapTo(mutableSetOf()) { it.getProjectTag(project) }
+            .ifEmpty { setOf(ProjectTag.OTHER) }
+
         projectsDataWriter.writer.println(
             listOf(
                 project.name.replace('#', '/'),
-                project.getTag().value
+                projectTags.joinToString(separator = ",")
             ).joinToString(separator = ",")
         )
-    }
-
-    /**
-     * If there is no gradle file in project, project tag is [ProjectTag.UNDEFINED];
-     * If there is gradle file contains [ANDROID_DEPENDENCY_GROUP_NAME], project tag is [ProjectTag.ANDROID];
-     * Other tags are not implemented now, so otherwise project tag is [ProjectTag.OTHER].
-     */
-    private fun Project.getTag(): ProjectTag {
-        val gradleFile = GradleFileManager.extractRootGradleFileFromProject(this)
-            ?: run {
-                logger.info("Can not find gradle file in project ${this.name}")
-                return ProjectTag.UNDEFINED
-            }
-
-        if (gradleFile.containsDependencyWithGroup(ANDROID_DEPENDENCY_GROUP_NAME)) {
-            logger.info("Project ${this.name} is android project")
-            return ProjectTag.ANDROID
-        }
-
-        return ProjectTag.OTHER
     }
 }
