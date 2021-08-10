6fc03491b17d1775b0256dd4f539d455da817713
tiginamaria <tiginamaria@gmail.com>
2021-08-11 00:02:17 +0300

    Add comments and example for gradle dependencies regexes

--- kotlin-analysis-gradle/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/gradle/GradleFileUtil.kt
+++ kotlin-analysis-gradle/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/gradle/GradleFileUtil.kt
@@ -10,16 +10,48 @@ class GradleFileUtil {
         private const val QUOTES = "[\'\"]"
         private const val SEPARATORS = "[\'\":,]+"
 
+        /**
+         * Regex to parse Gradle dependency:
+         *
+         * build.gradle:
+         * implementation "com.google.code.gson:gson:2.8.7" -- standard
+         * implementation 'com.google.code.gson:gson:2.8.7' -- single quotes
+         * implementation "com.google.code.gson:gson" -- without version
+         *
+         * build.gradle.kts:
+         * implementation("com.google.code.gson:gson:2.8.7") -- standard
+         * implementation("com.google.code.gson","gson","2.8.7") -- with comma separation
+         * ... same as for build.gradle but in build.gradle.kts notation with brackets ...
+         **/
         private val GRADLE_DEPENDENCIES_SHORT_REGEX = "(${
             GradleDependencyConfiguration.availableKeys().joinToString(separator = "|")
         })[(]?$QUOTES($NAME)$SEPARATORS($NAME)($SEPARATORS$NAME)?$QUOTES[)]?"
             .toRegex(RegexOption.IGNORE_CASE)
 
+        /**
+         * Regex to parse Gradle dependency:
+         *
+         * build.gradle:
+         * implementation group="com.google.code.gson",name="gson",version="2.8.7" -- standard
+         * implementation group='com.google.code.gson',name='gson',version='2.8.7' -- single quotes
+         * implementation group='com.google.code.gson',name='gson' -- without version
+         *
+         * build.gradle.kts:
+         * implementation(group="com.google.code.gson",name="gson",version="2.8.7") -- standard
+         * ... same as for build.gradle but in build.gradle.kts notation with brackets ...
+         **/
         private val GRADLE_DEPENDENCIES_FULL_REGEX = "(${
             GradleDependencyConfiguration.availableKeys().joinToString(separator = "|")
         })[(]?group=$QUOTES($NAME)$QUOTES,name=$QUOTES($NAME)$QUOTES(,version=$QUOTES$NAME$QUOTES)?[)]?"
             .toRegex(RegexOption.IGNORE_CASE)
 
+        /**
+         * Regex to parse Kotlin specific Gradle dependency, which is use short version for
+         * 'org.jetbrains.kotlin' libraries:
+         *
+         * build.gradle.kts:
+         * classpath(kotlin("gradle-plugin", "1.5.20")) ~ classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20"))
+         **/
         private val GRADLE_DEPENDENCIES_KOTLIN_REGEX = "(${
             GradleDependencyConfiguration.availableKeys().joinToString(separator = "|")
         })[(]?kotlin[(]?$QUOTES($NAME)$QUOTES.*[)]?[)]?"
@@ -32,6 +64,9 @@ class GradleFileUtil {
                         ?: GRADLE_DEPENDENCIES_FULL_REGEX.matchEntire(dependencyLine))
                         ?.groups
                         ?.let {
+                            if (it.size < 4) {
+                                return null
+                            }
                             val configKey = it[1]?.value ?: return null
                             val groupId = it[2]?.value ?: return null
                             val artifactId = it[3]?.value ?: return null
@@ -39,6 +74,9 @@ class GradleFileUtil {
                         } ?: (GRADLE_DEPENDENCIES_KOTLIN_REGEX.matchEntire(dependencyLine))
                         ?.groups
                         ?.let {
+                            if (it.size < 3) {
+                                return null
+                            }
                             val configKey = it[1]?.value ?: return null
                             val groupId = "org.jetbrains.kotlin"
                             val artifactId = it[2]?.value
