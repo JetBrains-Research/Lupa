8660af2faf76ca44e8e95bd774fedb2bf64db476
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-09-24 18:07:04 +0300

    Added support for relative imports

--- python-analysis-dependencies/src/main/kotlin/org/jetbrains/research/ml/pythonAnalysis/FromImportStatementPsiAnalyzer.kt
+++ python-analysis-dependencies/src/main/kotlin/org/jetbrains/research/ml/pythonAnalysis/FromImportStatementPsiAnalyzer.kt
@@ -1,20 +1,41 @@
 package org.jetbrains.research.ml.pythonAnalysis
 
+import com.jetbrains.extensions.getQName
 import com.jetbrains.python.psi.PyFromImportStatement
 import org.jetbrains.research.ml.kotlinAnalysis.PsiAnalyzer
 
 /**
- * Analyzer for [from import statement][PyFromImportStatement].
- * Analysis consists of fully qualified name extraction.
+ * Analyzer for [from import statement][PyFromImportStatement]. Analysis consists of fully qualified
+ * name extraction.
  */
 object FromImportStatementPsiAnalyzer : PsiAnalyzer<PyFromImportStatement, List<String>> {
 
     /** Get fully qualified name of given [from import statement][PyFromImportStatement]. */
     override fun analyze(psiElement: PyFromImportStatement): List<String> {
-        return if (psiElement.isStarImport) {
-            listOf(psiElement.importSourceQName.toString())
-        } else {
-            psiElement.fullyQualifiedObjectNames
+        return when (psiElement.relativeLevel) {
+            // Processing absolute import
+            0 -> {
+                when (psiElement.isStarImport) {
+                    true -> psiElement.importSourceQName?.toString()?.let { listOf(it) }
+                            ?: emptyList()
+                    false -> psiElement.fullyQualifiedObjectNames
+                }
+            }
+            // Processing relative import
+            else -> {
+                val importSourceQName =
+                    psiElement.resolveImportSource()?.getQName()?.toString()
+                        ?: psiElement.importSourceQName?.toString()
+
+                when (psiElement.isStarImport) {
+                    true -> importSourceQName?.let { listOf(it) } ?: emptyList()
+                    false -> {
+                        val importElements =
+                            psiElement.importElements.mapNotNull { it.importedQName?.toString() }
+                        importElements.map { "$importSourceQName.$it" }
+                    }
+                }
+            }
         }
     }
 }
