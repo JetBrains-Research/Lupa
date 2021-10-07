8da74e4d61bcc16f8af36e674d257b55b1867cb7
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-10-07 17:17:46 +0300

    Fixed package gathering

--- python-analysis-dependencies/src/main/kotlin/org/jetbrains/research/ml/pythonAnalysis/ImportStatementsAnalysisExecutor.kt
+++ python-analysis-dependencies/src/main/kotlin/org/jetbrains/research/ml/pythonAnalysis/ImportStatementsAnalysisExecutor.kt
@@ -1,7 +1,12 @@
 package org.jetbrains.research.ml.pythonAnalysis
 
 import com.intellij.openapi.project.Project
-import com.jetbrains.python.packaging.PyPackageUtil
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileVisitor
 import com.jetbrains.python.psi.PyFromImportStatement
 import com.jetbrains.python.psi.PyImportStatement
 import com.jetbrains.python.statistics.modules
@@ -27,19 +32,82 @@ class ImportStatementsAnalysisExecutor(outputDir: Path, filename: String = "impo
 
     override fun analyse(project: Project) {
         val importStatements = project.extractPyElementsOfType(PyImportStatement::class.java)
-        val fqNames = importStatements.map { ImportStatementPsiAnalyzer.analyze(it) }.flatten().toMutableSet()
+        val fqNames = importStatements.flatMap { ImportStatementPsiAnalyzer.analyze(it) }.toMutableSet()
 
         val fromImportStatements = project.extractPyElementsOfType(PyFromImportStatement::class.java)
-        fqNames.addAll(fromImportStatements.map { FromImportStatementPsiAnalyzer.analyze(it) }.flatten())
+        fqNames.addAll(
+            fromImportStatements.flatMap {
+                FromImportStatementPsiAnalyzer.analyze(
+                    it,
+                    ignoreRelativeImports = true,
+                )
+            }
+        )
 
-        val packageNames = project.modules.map { PyPackageUtil.getPackageNames(it) }.flatten().toSet()
+        val contentRoots = project.modules.flatMap { ModuleRootManager.getInstance(it).contentRoots.toList() }.toSet()
+        val packageRoots = contentRoots.flatMap { collectPackageRoots(project, it) }.toSet()
+        val packageNames = packageRoots.flatMap { collectPackageNames(project, it) }.toSet()
 
-        fqNames.retainAll { importName -> !packageNames.any { importName.startsWith(it) } }
+        fqNames.retainAll { importName -> !isLocalImport(importName, packageNames) }
 
         fqNames.ifNotEmpty {
             dependenciesDataWriter.writer.println(joinToString(separator = System.getProperty("line.separator")) {
                 listOf(project.name, it).joinToString(separator = ",")
             })
+        }
+    }
+
+    companion object {
+        private const val INIT_DOT_PY = "__init__.py"
+
+        private fun isPackage(file: VirtualFile, fileIndex: ProjectFileIndex): Boolean =
+            !fileIndex.isExcluded(file) && file.isDirectory && file.findChild(INIT_DOT_PY) != null
+
+        private fun collectPackageRoots(project: Project, contentRoot: VirtualFile): List<VirtualFile> {
+            val packageRoots = mutableListOf<VirtualFile>()
+
+            val fileIndex = ProjectRootManager.getInstance(project).fileIndex
+            VfsUtilCore.visitChildrenRecursively(contentRoot, object : VirtualFileVisitor<Unit>() {
+                override fun visitFile(file: VirtualFile): Boolean {
+                    return if (isPackage(file, fileIndex)) {
+                        packageRoots.add(file)
+                        false
+                    } else {
+                        true
+                    }
+                }
+            })
+
+            return packageRoots
+        }
+
+        private fun collectPackageNames(project: Project, packageRoot: VirtualFile): List<String> {
+            val packageNames = mutableListOf<String>()
+
+            val fileIndex = ProjectRootManager.getInstance(project).fileIndex
+            VfsUtilCore.visitChildrenRecursively(packageRoot, object : VirtualFileVisitor<Unit>() {
+                override fun visitFile(file: VirtualFile): Boolean {
+                    return if (file == packageRoot) {
+                        true
+                    } else if (isPackage(file, fileIndex)) {
+                        VfsUtilCore.getRelativePath(file, packageRoot, '.')
+                            ?.let { packageNames.add("${packageRoot.name}.${it}") }
+                        true
+                    } else {
+                        false
+                    }
+                }
+            })
+
+            packageNames.add(packageRoot.name)
+
+            return packageNames
+        }
+
+        private fun isLocalImport(importName: String, packageNames: Set<String>): Boolean {
+            val importNameWithDot = "$importName."
+            val packageNamesWithDot = packageNames.map { "$it." }
+            return packageNamesWithDot.any { packageNameWithDot -> importNameWithDot.startsWith(packageNameWithDot) }
         }
     }
 }
