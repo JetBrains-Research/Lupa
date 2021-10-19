2223b267ef9fb1855f41af263fd247345bf50aaf
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-10-19 18:36:22 +0300

    Fixed PR issue

---
+++ kotlin-analysis-core/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/psi/extentions/PyElementExt.kt
@@ -0,0 +1,14 @@
+package org.jetbrains.research.ml.kotlinAnalysis.psi.extentions
+
+import com.jetbrains.python.psi.PyFromImportStatement
+
+
+private const val RELATIVE_LEVEL_OF_ABSOLUTE_IMPORT = 0
+
+/**
+ * Checks if the import is absolute.
+ *
+ * For example, `from some_package import some_function` is absolute
+ * and `from .some_other_package import some_other_function` is not absolute.
+ */
+fun PyFromImportStatement.isAbsoluteImport(): Boolean = this.relativeLevel == RELATIVE_LEVEL_OF_ABSOLUTE_IMPORT
--- python-analysis-dependencies/src/main/kotlin/org/jetbrains/research/ml/pythonAnalysis/FromImportStatementPsiAnalyzer.kt
+++ python-analysis-dependencies/src/main/kotlin/org/jetbrains/research/ml/pythonAnalysis/FromImportStatementPsiAnalyzer.kt
@@ -3,46 +3,42 @@ package org.jetbrains.research.ml.pythonAnalysis
 import com.jetbrains.extensions.getQName
 import com.jetbrains.python.psi.PyFromImportStatement
 import org.jetbrains.research.ml.kotlinAnalysis.PsiAnalyzer
+import org.jetbrains.research.ml.kotlinAnalysis.psi.extentions.isAbsoluteImport
 
 /**
  * Analyzer for [from import statement][PyFromImportStatement]. Analysis consists of fully qualified
  * name extraction.
  */
 object FromImportStatementPsiAnalyzer : PsiAnalyzer<PyFromImportStatement, List<String>> {
-
-    private const val RELATIVE_LEVEL_OF_ABSOLUTE_IMPORT = 0
-
     /** Get fully qualified name of given [from import statement][PyFromImportStatement]. */
     override fun analyze(psiElement: PyFromImportStatement): List<String> {
-        return when (psiElement.relativeLevel) {
+        return if (psiElement.isAbsoluteImport()) {
             // Processing absolute import
-            RELATIVE_LEVEL_OF_ABSOLUTE_IMPORT -> {
-                if (psiElement.isStarImport) {
-                    psiElement.importSourceQName?.toString()?.let { listOf(it) } ?: emptyList()
-                } else {
-                    psiElement.fullyQualifiedObjectNames
-                }
+            if (psiElement.isStarImport) {
+                psiElement.importSourceQName?.toString()?.let { listOf(it) } ?: emptyList()
+            } else {
+                psiElement.fullyQualifiedObjectNames
             }
+        } else {
             // Processing relative import
-            else -> {
-                val importSourceQName = psiElement.resolveImportSource()?.getQName()?.toString()
+            val importSourceQName = psiElement.resolveImportSource()?.getQName()?.toString()
 
-                if (psiElement.isStarImport) {
-                    importSourceQName?.let { listOf(it) } ?: emptyList()
-                } else {
-                    val importElements = psiElement.importElements.mapNotNull { it.importedQName?.toString() }
-                    importSourceQName?.let { importSource ->
-                        importElements.map { importElement -> "$importSource.$importElement" }
-                    } ?: emptyList()
-                }
+            if (psiElement.isStarImport) {
+                importSourceQName?.let { listOf(it) } ?: emptyList()
+            } else {
+                val importElements = psiElement.importElements.mapNotNull { it.importedQName?.toString() }
+                importSourceQName?.let { importSource ->
+                    importElements.map { importElement -> "$importSource.$importElement" }
+                } ?: emptyList()
             }
+
         }
     }
 
     /** Get fully qualified name of given [from import statement][PyFromImportStatement].
      * If [ignoreRelativeImports] is enabled, relative imports will be ignored. */
     fun analyze(psiElement: PyFromImportStatement, ignoreRelativeImports: Boolean): List<String> {
-        if (ignoreRelativeImports && psiElement.relativeLevel != RELATIVE_LEVEL_OF_ABSOLUTE_IMPORT) {
+        if (ignoreRelativeImports && !psiElement.isAbsoluteImport()) {
             return emptyList()
         }
 
