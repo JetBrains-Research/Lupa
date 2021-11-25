69a3e686e948e0be494db3f0b83a00275b47058e
Ilya Vlasov <55441714+GirZ0n@users.noreply.github.com>
2021-11-25 14:10:34 +0300

    Python venv creation script (#45)
    * Add a script to create a virtual environment
    
    * Fix flake8 issue
    
    * Fix typos
    
    * 1) Add filter_unavailable_packages function
    2) Add _create_session function
    3) Add more logging
    
    * Fix flake8 issue
    
    * Add documentation
    
    * Add extra line
    
    * Fix bug
    
    * Add some tests
    
    * Add python tests
    
    * Fix python tests
    
    * Fix bug
    
    * Add test_filter_unavailable_packages
    
    * Added test requirements
    
    * Fix python tests
    
    * Fix test_filter_unavailable_packages
    
    * Fix bug
    
    * Add test_get_available_versions
    
    * Add _httpretty_fixture
    
    * Add test_filter_unavailable_versions
    
    * Fix cache
    
    * Fix python tests
    
    * Add new tests
    
    * Add S101
    
    * Fix naming
    
    * Fix naming
    
    * Code refactoring:
    1) Fix typos.
    2) Fix the number of retries and the backoff factor of retries.
    3) Fix naming.
    
    * Fix naming
    
    * Fix flake8 issue
    
    * Add some description what happens if the one library has different versions in the different projects.
    
    * Create kotlin-build.yml
    
    * Create python-build.yml
    
    * Delete build.yml
    
    * Fix name
    
    * Update python-build.yml
    
    * Fix typos

---
+++ scripts/plugin_runner/create_venv.py
@@ -0,0 +1,316 @@
+"""
+This script allows you to create a virtual environment
+and install the requirements gathered from a given dataset with python projects.
+
+It accepts
+    * path to the folder with python projects.
+    * path to the folder where you want to create the environment.
+    * flag that allows you not to do version validation using PyPI.
+    * flag that allows you not to do package name validation using PyPI.
+    * flag that allows you not to install dependencies for each package (--no-deps flag for pip).
+
+In the current version of the script, if we find different versions of the same library
+in different requirement files, we will choose the newest (largest) version.
+"""
+
+import argparse
+import json
+import logging
+import subprocess
+import sys
+from collections import defaultdict
+from copy import copy
+from distutils.version import Version
+from pathlib import Path
+from typing import Dict, Optional, Set, Tuple
+
+import pkg_resources
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+
+PYPI_PACKAGE_METADATA_URL = 'https://pypi.org/pypi/{package_name}/json'
+REQUIREMENTS_FILE_NAME_REGEXP = '*requirements*.txt'
+
+Specs = Set[Tuple[str, Version]]
+Requirements = Dict[str, Specs]
+
+logger = logging.getLogger(__name__)
+
+
+def configure_arguments(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument(
+        'dataset_path',
+        help='Path to dataset with projects from which you want to get requirements and create a virtual environment.',
+        type=lambda value: Path(value).absolute(),
+    )
+
+    parser.add_argument(
+        'venv_path',
+        help='Path to the folder where you want to create the virtual environment.',
+        type=lambda value: Path(value).absolute(),
+    )
+
+    parser.add_argument(
+        '--no-package-name-validation',
+        help='If specified, no package name validation will be performed using PyPI.',
+        action='store_true',
+    )
+
+    parser.add_argument(
+        '--no-version-validation',
+        help='If specified, no version validation will be performed using PyPI.',
+        action='store_true',
+    )
+
+    parser.add_argument(
+        '--no-package-dependencies',
+        help=(
+            'If specified, no dependencies will be installed for each package '
+            '(the --no-deps flag will be passed to pip).'
+        ),
+        action='store_true',
+    )
+
+
+def gather_requirements(dataset_path: Path) -> Requirements:
+    """
+    Collects requirements from all projects.
+
+    :param dataset_path: the path to the folder with the projects that contain the requirements files.
+    :return: dictionary, where for each package the collected specs are listed.
+             The spec is a pair of operator and version.
+    """
+
+    logger.info('Collecting requirements.')
+
+    requirements = defaultdict(set)
+    for file_path in dataset_path.rglob(REQUIREMENTS_FILE_NAME_REGEXP):
+        with open(file_path) as file:
+            try:
+                file_requirements = list(pkg_resources.parse_requirements(file))
+            except Exception:
+                # For some reason you can't catch RequirementParseError (or InvalidRequirement), so we catch Exception.
+                logger.info(f'Unable to parse {str(file_path)}. Skipping.')
+                continue
+
+            for requirement in file_requirements:
+                specs = {(operator, pkg_resources.parse_version(version)) for operator, version in requirement.specs}
+                requirements[requirement.key] |= specs
+
+    logger.info(f'Collected {len(requirements)} packages.')
+
+    return requirements
+
+
+def _create_session() -> requests.Session:
+    session = requests.Session()
+    retries = Retry(total=10, backoff_factor=0.1)
+    session.mount('https://', HTTPAdapter(max_retries=retries))
+    return session
+
+
+def filter_unavailable_packages(requirements: Requirements) -> Requirements:
+    """
+    Removes all package names that are not on PyPI.
+
+    :param requirements: dictionary, where for each package the specs are listed.
+                         The spec is a pair of operator and version.
+    :return: dictionary, where for each package the specs are listed. The spec is a pair of operator and version.
+    """
+
+    logger.info('Filtering unavailable packages.')
+
+    session = _create_session()
+    filtered_requirements = copy(requirements)
+    for package_name in requirements.keys():
+        logger.info(f'Checking {package_name}.')
+
+        try:
+            response = session.get(PYPI_PACKAGE_METADATA_URL.format(package_name=package_name))
+
+            if response.status_code == 200:
+                continue
+
+            if response.status_code == 404:
+                logger.warning(
+                    f'The {package_name} package does not exist on PyPI. Removing the package from requirements.',
+                )
+                filtered_requirements.pop(package_name)
+            else:
+                logger.warning(
+                    f'PyPI returned an unexpected code ({response.status_code}). Skipping.',
+                )
+
+        except requests.exceptions.RequestException:
+            logger.error('An error occurred when accessing the PyPI. Skipping.')
+
+    logger.info(f'Filtered {len(requirements) - len(filtered_requirements)} packages.')
+    return filtered_requirements
+
+
+def _get_available_versions(package_name: str) -> Set[Version]:
+    """
+    By a given package, collects a list of all the versions available on PyPI.
+
+    :param package_name: PyPI package name.
+    :return: set of available versions. If the version could not be obtained, None will be returned.
+    """
+
+    session = _create_session()
+    url = PYPI_PACKAGE_METADATA_URL.format(package_name=package_name)
+    try:
+        metadata = session.get(url).json()
+    except requests.exceptions.RequestException:
+        logger.error('An error occurred when accessing the PyPI. Skipping.')
+        return set()
+    except json.JSONDecodeError:
+        logger.error(f'Failed to get a version for the {package_name} package. Skipping')
+        return set()
+
+    if 'releases' in metadata.keys():
+        versions = metadata['releases'].keys()
+    else:
+        logger.error('The PyPI response does not contain the "releases" field. Skipping.')
+        return set()
+
+    return set(map(pkg_resources.parse_version, versions))
+
+
+def filter_unavailable_versions(specs: Requirements) -> Requirements:
+    """
+    Removes all versions of packages that are not on PyPI.
+
+    :param specs: dictionary, where for each package the specs are listed.
+                  The spec is a pair of operator and version.
+    :return: dictionary, where for each package the specs are listed. The spec is a pair of operator and version.
+             The specs contain only those versions which are available on the PyPI.
+    """
+
+    logger.info('Filtering unavailable versions.')
+
+    filtered_requirements = {}
+    for package_name, specs in specs.items():
+        logger.info(f'Checking {package_name} versions.')
+
+        available_versions = _get_available_versions(package_name)
+
+        filtered_specs = specs
+        if available_versions:
+            filtered_specs = {
+                (operator, version) for operator, version in filtered_specs if version in available_versions
+            }
+        else:
+            logger.warning(f'Unable to check {package_name} versions.')
+
+        filtered_requirements[package_name] = filtered_specs
+
+    return filtered_requirements
+
+
+def merge_requirements(requirements: Requirements) -> Dict[str, Optional[Version]]:
+    """
+    For each package leaves only the highest version of requirements.
+
+    :param requirements: dictionary, where for each package the specs are listed.
+                         The spec is a pair of operator and version.
+    :return: dictionary, where a version is specified for each package.
+    """
+
+    logger.info('Merging requirements.')
+
+    version_by_package_name = {}
+    for package_name, specs in requirements.items():
+        max_version = None
+        if specs:
+            max_version = max(version for _, version in specs)
+        version_by_package_name[package_name] = max_version
+    return version_by_package_name
+
+
+def create_requirements_file(version_by_package_name: Dict[str, Optional[Version]], requirements_dir: Path) -> Path:
+    """
+    Creates a requirements file from the passed dictionary.
+
+    :param version_by_package_name: dictionary, where for each package, specifies the version to be installed.
+    :param requirements_dir: the path where the requirements file will be created.
+    :return: the path to the created requirements file.
+    """
+
+    logger.info('Creating requirements file.')
+
+    requirements_dir.mkdir(exist_ok=True, parents=True)
+    path_to_requirements = requirements_dir / 'requirements.txt'
+
+    with open(path_to_requirements, mode='w+') as file:
+        for package_name, version in version_by_package_name.items():
+            if version is None:
+                file.write(f'{package_name}\n')
+            else:
+                file.write(f'{package_name}=={version}\n')
+
+    return path_to_requirements
+
+
+def create_venv(venv_path: Path, requirements_path: Path, no_package_dependencies: bool) -> int:
+    """
+    In the passed path creates a virtual environment and installs the passed requirements.
+
+    :param venv_path: the path where the virtual environment will be created.
+    :param requirements_path: the path to the requirements file.
+    :param no_package_dependencies: whether it is necessary to not install dependencies for each package.
+    :return: pip return code
+    """
+
+    logger.info('Creating virtual environment.')
+
+    venv_path.mkdir(exist_ok=True, parents=True)
+    pip_path = venv_path / 'bin' / 'pip'
+
+    subprocess.run(
+        [
+            'python3',
+            '-m',
+            'venv',
+            str(venv_path),
+        ],
+    )
+
+    pip_command = [
+        str(pip_path),
+        'install',
+        '-r',
+        str(requirements_path),
+    ]
+
+    if no_package_dependencies:
+        pip_command.append('--no-deps')
+
+    return subprocess.run(pip_command).returncode
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    configure_arguments(parser)
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s | %(message)s')
+
+    args = parser.parse_args()
+
+    requirements = gather_requirements(args.dataset_path)
+
+    if not args.no_package_name_validation:
+        requirements = filter_unavailable_packages(requirements)
+
+    if not args.no_version_validation:
+        requirements = filter_unavailable_versions(requirements)
+
+    version_by_package_name = merge_requirements(requirements)
+    requirements_path = create_requirements_file(version_by_package_name, args.venv_path)
+    exit_code = create_venv(args.venv_path, requirements_path, args.no_package_dependencies)
+
+    return exit_code
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ scripts/test/plugin_runner/test_create_venv.py
@@ -0,0 +1,373 @@
+import tempfile
+from distutils.version import Version
+from pathlib import Path
+from typing import Dict, List, Optional, Set
+
+import httpretty
+import pkg_resources
+import pytest as pytest
+
+from plugin_runner.create_venv import (
+    PYPI_PACKAGE_METADATA_URL,
+    Requirements,
+    _get_available_versions,
+    create_requirements_file,
+    filter_unavailable_packages,
+    filter_unavailable_versions,
+    gather_requirements,
+    merge_requirements,
+)
+from test.plugin_runner import CREATE_VENV_TEST_DATA_FOLDER
+
+
+@pytest.fixture
+def _httpretty_fixture():
+    httpretty.enable(allow_net_connect=False)
+    yield
+    httpretty.disable()
+
+
+GATHER_REQUIREMENTS_TEST_DATA = [
+    ('project_with_incorrect_requirement_file', {}),
+    ('project_without_requirements_files', {}),
+    ('project_with_empty_requirements_file', {}),
+    (
+        'project_with_several_requirements_files',
+        {
+            'numpy': set(zip(['==', '=='], map(pkg_resources.parse_version, ['1.2.3', '3.2.1']))),
+            'pandas': set(zip(['==', '=='], map(pkg_resources.parse_version, ['4.5.6', '6.5.4']))),
+            'plotly': set(),
+        },
+    ),
+    (
+        'project_with_nested_structure',
+        {
+            'numpy': set(zip(['==', '=='], map(pkg_resources.parse_version, ['1.2.3', '3.2.1']))),
+            'pandas': set(zip(['==', '=='], map(pkg_resources.parse_version, ['4.5.6', '6.5.4']))),
+            'plotly': set(),
+        },
+    ),
+]
+
+
+@pytest.mark.parametrize(('folder_name', 'expected_requirements'), GATHER_REQUIREMENTS_TEST_DATA)
+def test_gather_requirements(folder_name: str, expected_requirements: Requirements):
+    assert expected_requirements == gather_requirements(CREATE_VENV_TEST_DATA_FOLDER / folder_name)
+
+
+FILTER_UNAVAILABLE_PACKAGES_TEST_DATA = [
+    (
+        {'numpy': 200, 'pandas': 200},
+        {
+            'numpy': {('==', pkg_resources.parse_version('1.2.3'))},
+            'pandas': {('==', pkg_resources.parse_version('4.5.6'))},
+        },
+        {
+            'numpy': {('==', pkg_resources.parse_version('1.2.3'))},
+            'pandas': {('==', pkg_resources.parse_version('4.5.6'))},
+        },
+    ),
+    (
+        {'numpy': 404, 'pandas': 200},
+        {
+            'numpy': {('==', pkg_resources.parse_version('1.2.3'))},
+            'pandas': {('==', pkg_resources.parse_version('4.5.6'))},
+        },
+        {
+            'pandas': {('==', pkg_resources.parse_version('4.5.6'))},
+        },
+    ),
+    (
+        {'numpy': 404, 'pandas': 404},
+        {
+            'numpy': {('==', pkg_resources.parse_version('1.2.3'))},
+            'pandas': {('==', pkg_resources.parse_version('4.5.6'))},
+        },
+        {},
+    ),
+    (
+        {'numpy': 505},  # Unexpected code
+        {'numpy': {('==', pkg_resources.parse_version('1.2.3'))}},
+        {'numpy': {('==', pkg_resources.parse_version('1.2.3'))}},
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    ('status_code_by_package_name', 'requirements', 'expected_requirements'),
+    FILTER_UNAVAILABLE_PACKAGES_TEST_DATA,
+)
+def test_filter_unavailable_packages(
+    _httpretty_fixture,
+    status_code_by_package_name: Dict[str, int],
+    requirements: Requirements,
+    expected_requirements: Requirements,
+):
+    for package_name, status_code in status_code_by_package_name.items():
+        httpretty.register_uri(
+            httpretty.GET,
+            PYPI_PACKAGE_METADATA_URL.format(package_name=package_name),
+            status=status_code,
+        )
+
+    assert expected_requirements == filter_unavailable_packages(requirements)
+
+
+GET_AVAILABLE_VERSIONS_TEST_DATA = [
+    (
+        'numpy',
+        """
+        {
+            "releases": {
+                "1.2.3": {},
+                "3.4.5": {},
+                "6.7.8": {}
+            }
+        }
+        """,
+        set(map(pkg_resources.parse_version, ['1.2.3', '3.4.5', '6.7.8'])),
+    ),
+    (
+        'numpy',
+        """
+        {
+            "releases": {}
+        }
+        """,
+        set(),
+    ),
+    (
+        'numpy',
+        'This is not a json.',
+        set(),
+    ),
+    (
+        'numpy',
+        """
+        {
+            "incorrect_key": {
+                "1.2.3": {},
+                "3.4.5": {},
+                "6.7.8": {}
+            }
+        }
+        """,
+        set(),
+    ),
+]
+
+
+@pytest.mark.parametrize(('package_name', 'response_json', 'expected_versions'), GET_AVAILABLE_VERSIONS_TEST_DATA)
+def test_get_available_versions(
+    _httpretty_fixture,
+    package_name: str,
+    response_json: str,
+    expected_versions: Set[Version],
+):
+    httpretty.register_uri(
+        httpretty.GET,
+        PYPI_PACKAGE_METADATA_URL.format(package_name=package_name),
+        body=response_json,
+    )
+
+    assert expected_versions == _get_available_versions(package_name)
+
+
+FILTER_UNAVAILABLE_VERSIONS_TEST_DATA = [
+    (
+        {
+            'numpy': """
+            {
+                "releases": {
+                    "1.2.3": {},
+                    "3.2.1": {}
+                }
+            }
+            """,
+            'pandas': """
+            {
+                "releases": {
+                    "4.5.6": {},
+                    "6.5.4": {}
+                }
+            }
+            """,
+        },
+        {
+            'numpy': set(zip(['==', '=='], map(pkg_resources.parse_version, ['1.2.3', '3.2.1']))),
+            'pandas': set(zip(['==', '=='], map(pkg_resources.parse_version, ['4.5.6', '6.5.4']))),
+        },
+        {
+            'numpy': set(zip(['==', '=='], map(pkg_resources.parse_version, ['1.2.3', '3.2.1']))),
+            'pandas': set(zip(['==', '=='], map(pkg_resources.parse_version, ['4.5.6', '6.5.4']))),
+        },
+    ),
+    (
+        {
+            'numpy': """
+        {
+            "releases": {
+                "1.2.3": {}
+            }
+        }
+        """,
+            'pandas': """
+        {
+            "releases": {
+                "4.5.6": {},
+                "6.5.4": {}
+            }
+        }
+        """,
+        },
+        {
+            'numpy': set(zip(['==', '=='], map(pkg_resources.parse_version, ['1.2.3', '3.2.1']))),
+            'pandas': set(zip(['==', '=='], map(pkg_resources.parse_version, ['4.5.6', '6.5.4']))),
+        },
+        {
+            'numpy': {('==', pkg_resources.parse_version('1.2.3'))},
+            'pandas': set(zip(['==', '=='], map(pkg_resources.parse_version, ['4.5.6', '6.5.4']))),
+        },
+    ),
+    (
+        {
+            'numpy': """
+        {
+            "releases": {
+                "1.2.3": {},
+                "3.2.1": {}
+            }
+        }
+        """,
+            'pandas': """
+        {
+            "releases": {
+                "4.5.6": {},
+                "6.5.4": {}
+            }
+        }
+        """,
+        },
+        {
+            'numpy': set(zip(['==', '=='], map(pkg_resources.parse_version, ['1.2.3', '3.2.1']))),
+            'pandas': set(zip(['==', '=='], map(pkg_resources.parse_version, ['7.8.9', '9.8.7']))),
+        },
+        {
+            'numpy': set(zip(['==', '=='], map(pkg_resources.parse_version, ['1.2.3', '3.2.1']))),
+            'pandas': set(),
+        },
+    ),
+    (
+        {
+            'numpy': """
+        {
+            "releases": {
+                "1.2.3": {},
+                "3.2.1": {}
+            }
+        }
+        """,
+            'pandas': """
+        {
+            "releases": {}
+        }
+        """,
+        },
+        {
+            'numpy': set(zip(['==', '=='], map(pkg_resources.parse_version, ['1.2.3', '3.2.1']))),
+            'pandas': set(zip(['==', '=='], map(pkg_resources.parse_version, ['4.5.6', '6.5.4']))),
+        },
+        {
+            'numpy': set(zip(['==', '=='], map(pkg_resources.parse_version, ['1.2.3', '3.2.1']))),
+            'pandas': set(zip(['==', '=='], map(pkg_resources.parse_version, ['4.5.6', '6.5.4']))),
+        },
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    ('json_response_by_package_name', 'requirements', 'expected_requirements'),
+    FILTER_UNAVAILABLE_VERSIONS_TEST_DATA,
+)
+def test_filter_unavailable_versions(
+    _httpretty_fixture,
+    json_response_by_package_name: Dict[str, str],
+    requirements: Requirements,
+    expected_requirements: Requirements,
+):
+    for package_name, json_response in json_response_by_package_name.items():
+        httpretty.register_uri(
+            httpretty.GET,
+            PYPI_PACKAGE_METADATA_URL.format(package_name=package_name),
+            body=json_response,
+        )
+
+    assert expected_requirements == filter_unavailable_versions(requirements)
+
+
+MERGE_REQUIREMENTS_TEST_DATA = [
+    (
+        {
+            'numpy': set(zip(['==', '=='], map(pkg_resources.parse_version, ['1.2.3', '3.2.1']))),
+            'pandas': set(zip(['==', '=='], map(pkg_resources.parse_version, ['4.5.6', '6.5.4']))),
+        },
+        {
+            'numpy': pkg_resources.parse_version('3.2.1'),
+            'pandas': pkg_resources.parse_version('6.5.4'),
+        },
+    ),
+    (
+        {
+            'numpy': set(),
+            'pandas': set(zip(['==', '=='], map(pkg_resources.parse_version, ['4.5.6', '6.5.4']))),
+        },
+        {
+            'numpy': None,
+            'pandas': pkg_resources.parse_version('6.5.4'),
+        },
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    ('requirements', 'expected_version_by_package_name'),
+    MERGE_REQUIREMENTS_TEST_DATA,
+)
+def test_merge_requirements(
+    requirements: Requirements,
+    expected_version_by_package_name: Dict[str, Optional[Version]],
+):
+    assert expected_version_by_package_name == merge_requirements(requirements)
+
+
+CREATE_REQUIREMENTS_FILES_TEST_DATA = [
+    (
+        {
+            'numpy': pkg_resources.parse_version('3.2.1'),
+            'pandas': pkg_resources.parse_version('6.5.4'),
+        },
+        [
+            'numpy==3.2.1',
+            'pandas==6.5.4',
+        ],
+    ),
+    (
+        {
+            'numpy': None,
+            'pandas': pkg_resources.parse_version('6.5.4'),
+        },
+        [
+            'numpy',
+            'pandas==6.5.4',
+        ],
+    ),
+]
+
+
+@pytest.mark.parametrize(('version_by_package_name', 'expected_lines'), CREATE_REQUIREMENTS_FILES_TEST_DATA)
+def test_create_requirements_file(version_by_package_name: Dict[str, Optional[Version]], expected_lines: List[str]):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        requirements_file_path = create_requirements_file(version_by_package_name, Path(temp_dir))
+        with open(requirements_file_path) as requirements_file:
+            actual_lines = list(map(str.strip, requirements_file.readlines()))
+            assert actual_lines == expected_lines
