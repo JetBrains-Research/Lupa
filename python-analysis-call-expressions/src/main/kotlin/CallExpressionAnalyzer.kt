49a3fdd2d9f7bc1af41dc67b391d9c7ae9d6c01a
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-11-26 16:59:32 +0300

    Fix bug

--- python-analysis-call-expressions/src/main/kotlin/CallExpressionAnalyzer.kt
+++ python-analysis-call-expressions/src/main/kotlin/CallExpressionAnalyzer.kt
@@ -1,17 +1,20 @@
 package org.jetbrains.research.ml.pythonAnalysis
 
-import com.jetbrains.python.psi.PyCallExpression
+import com.jetbrains.python.psi.impl.PyCallExpressionImpl
 import org.jetbrains.research.ml.kotlinAnalysis.PsiAnalyzerWithContextImpl
 
 /**
- * Analyzer for [call expression][PyCallExpression].
+ * Analyzer for [call expression][PyCallExpressionImpl].
  * Analysis consists of fully qualified name extraction.
  */
 object CallExpressionAnalyzer :
-    PsiAnalyzerWithContextImpl<PyCallExpression, CallExpressionAnalyzerContext, String>(PyCallExpression::class.java) {
+    PsiAnalyzerWithContextImpl<PyCallExpressionImpl, CallExpressionAnalyzerContext, String>(PyCallExpressionImpl::class.java) {
 
-    /** Get fully qualified name of given [call expression][PyCallExpression]. **/
-    override fun analyzeWithContext(psiElement: PyCallExpression, context: CallExpressionAnalyzerContext?): String? {
+    /** Get fully qualified name of given [call expression][PyCallExpressionImpl]. **/
+    override fun analyzeWithContext(
+        psiElement: PyCallExpressionImpl,
+        context: CallExpressionAnalyzerContext?
+    ): String? {
         return context?.run {
             fqNamesProvider.getQualifiedName(psiElement.multiResolveCalleeFunction(resolveContext).firstOrNull())
         }
