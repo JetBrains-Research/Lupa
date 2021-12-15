4a9a07759244e9e5b81cd03ac49f2b02dff1be9c
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-12-15 17:41:51 +0300

    Fix CallExpressionPsiAnalyzerTest

---
+++ python-analysis-call-expressions/src/test/resources/org/jetbrains/research/ml/pythonAnalysis/psi/callExpressionPsiAnalyzerTestData/in_0_builtins.py
@@ -0,0 +1,110 @@
+# https://docs.python.org/3.8/library/functions.html
+
+# A
+abs()
+all()
+any()
+ascii()
+
+# B
+bin()
+bool()  # class
+breakpoint()
+bytearray()  # class
+bytes()  # class
+
+# C
+callable()
+chr()
+classmethod()  # class
+compile()
+complex()  # class
+
+# D
+delattr()
+dict()  # class
+dir()
+divmod()
+
+# E
+enumerate()  # class
+eval()
+exec()
+
+# F
+filter()  # class
+float()  # class
+format()
+frozenset() # class
+
+# G
+getattr()
+globals()
+
+# H
+hasattr()
+hash()
+help()
+hex()
+
+# I
+id()
+input()
+int()  # class
+isinstance()
+issubclass()
+iter()
+
+# L
+len()
+list()  # class
+locals()
+
+# M
+map()  # class
+max()
+memoryview()  # class
+min()
+
+# N
+next()
+
+# O
+object()  # class
+oct()
+open()
+ord()
+
+# P
+pow()
+print()
+property()  # class
+
+# R
+range()  # class
+repr()
+reversed()  # class
+round()
+
+# S
+set()  # class
+setattr()
+slice()  # class
+sorted()
+staticmethod()  # class
+str()  # class
+sum()
+super()  # class
+
+# T
+tuple()  # class
+type()  # class
+
+# V
+vars()
+
+# Z
+zip()  # class
+
+# _
+__import__()
--- python-analysis-call-expressions/src/test/resources/org/jetbrains/research/ml/pythonAnalysis/psi/callExpressionPsiAnalyzerTestData/in_1.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-
-np.zeros(42)
