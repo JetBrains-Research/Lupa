1695c0b39911014066a272c4bd62c2df61ec9219
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-12-26 13:17:29 +0300

    Fix test

--- python-analysis-call-expressions/src/test/kotlin/org/jetbrains/research/ml/pythonAnalysis/CallExpressionsAnalysisExecutorTest.kt
+++ python-analysis-call-expressions/src/test/kotlin/org/jetbrains/research/ml/pythonAnalysis/CallExpressionsAnalysisExecutorTest.kt
@@ -1,18 +1,18 @@
 package org.jetbrains.research.ml.pythonAnalysis
 
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.ProjectJdkTable
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.roots.ProjectRootManager
 import org.jetbrains.research.ml.kotlinAnalysis.util.ParametrizedGoldenFileTest
+import org.jetbrains.research.ml.kotlinAnalysis.util.RepositoryOpenerUtil
 import org.jetbrains.research.pluginUtilities.sdk.PythonMockSdk
 import org.jetbrains.research.pluginUtilities.sdk.SdkConfigurer
 import org.jetbrains.research.pluginUtilities.util.Extension
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
-import java.io.File
-import java.util.concurrent.TimeUnit
 
 @RunWith(Parameterized::class)
 open class CallExpressionsAnalysisExecutorTest : ParametrizedGoldenFileTest(
@@ -34,36 +34,11 @@ open class CallExpressionsAnalysisExecutorTest : ParametrizedGoldenFileTest(
         )
     }
 
-    private val venvScriptPath = "../scripts/plugin_runner/create_venv.py"
-    private val venvPath = File("$testDataPath/venv/")
-
-    private fun setupSdk() {
-        val project = myFixture.project
+    private fun setupSdk(project: Project) {
         val projectManager = ProjectRootManager.getInstance(project)
-        sdk = PythonMockSdk(testDataPath).create("3.8")
+        val sdk = PythonMockSdk(testDataPath).create("3.8")
         val sdkConfigurer = SdkConfigurer(project, projectManager)
         sdkConfigurer.setProjectSdk(sdk)
-    }
-
-    override fun setUp() {
-        super.setUp()
-
-        venvPath.mkdir()
-
-        val command = arrayOf(
-            "python3",
-            File(venvScriptPath).absolutePath,
-            getResourcesRootPath(::CallExpressionsAnalysisExecutorTest, "callExpressionsAnalysisExecutorData"),
-            venvPath.toString(),
-        )
-
-        ProcessBuilder(*command)
-            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-            .redirectError(ProcessBuilder.Redirect.INHERIT)
-            .start()
-            .waitFor(60, TimeUnit.MINUTES)
-
-        setupSdk()
     }
 
     override fun tearDown() {
@@ -71,15 +46,22 @@ open class CallExpressionsAnalysisExecutorTest : ParametrizedGoldenFileTest(
             ProjectJdkTable.getInstance().removeJdk(sdk)
         }
         super.tearDown()
-        venvPath.deleteRecursively()
     }
 
     @Test
     fun testCallExpressionsFqNamesInProject() {
         assertOutEqualsToGolden { inFile, outFile ->
             val analysisExecutor =
-                CallExpressionsAnalysisExecutor(outFile.parentFile.toPath(), outFile.name, venv = venvPath.toPath())
-            analysisExecutor.execute(inFile.toPath())
+                CallExpressionsAnalysisExecutor(outFile.parentFile.toPath(), outFile.name, venv = null)
+
+            analysisExecutor.controlledResourceManagers.map { it.init() }
+
+            RepositoryOpenerUtil.standardRepositoryOpener(inFile.toPath()) { project ->
+                setupSdk(project)
+                analysisExecutor.analyse(project)
+            }
+
+            analysisExecutor.controlledResourceManagers.map { it.close() }
         }
     }
 }
