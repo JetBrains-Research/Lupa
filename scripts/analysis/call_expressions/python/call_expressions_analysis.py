e834ac2a43433894a0e7526b9489f8a7fd3b7387
Ilya Vlasov <ilyavlasov2011@gmail.com>
2022-02-09 17:56:25 +0300

    Fix script

--- scripts/analysis/call_expressions/python/call_expressions_analysis.py
+++ scripts/analysis/call_expressions/python/call_expressions_analysis.py
@@ -1,9 +1,12 @@
 import argparse
+import logging
 from pathlib import Path
 
 import pandas as pd
 
 from call_expressions_column import CallExpressionsColumn
+
+logging.basicConfig(level=logging.INFO)
 
 
 def configure_parser(parser: argparse.ArgumentParser):
@@ -29,16 +32,30 @@ def main():
     input_path: Path = args.input
     output_path: Path = args.output
 
-    fq_names = pd.read_csv(input_path)
+    fq_names = pd.read_csv(input_path, keep_default_na=False)
+    logging.info(f'Received {len(fq_names)} FQ names.')
 
-    stats = fq_names.groupby(CallExpressionsColumn.FQ_NAME.value).aggregate(
-        {CallExpressionsColumn.FQ_NAME.value: 'count', CallExpressionsColumn.CATEGORY.value: 'first'},
+    total_stats = fq_names.drop(CallExpressionsColumn.CATEGORY.value, axis=1).drop_duplicates()
+    total_stats = total_stats.value_counts([CallExpressionsColumn.FQ_NAME.value])
+    total_stats = total_stats.reset_index(name=CallExpressionsColumn.TOTAL.value)
+
+    grouped_stats = fq_names.groupby([CallExpressionsColumn.FQ_NAME.value, CallExpressionsColumn.CATEGORY.value])
+    stats = grouped_stats[CallExpressionsColumn.PROJECT_NAME.value].count()
+    stats = stats.reset_index(name=CallExpressionsColumn.COUNT.value)
+    stats = stats.pivot(
+        index=CallExpressionsColumn.FQ_NAME.value,
+        columns=CallExpressionsColumn.CATEGORY.value,
+        values=CallExpressionsColumn.COUNT.value,
     )
-    stats.rename(columns={CallExpressionsColumn.FQ_NAME.value: CallExpressionsColumn.COUNT.value}, inplace=True)
-    stats.reset_index(CallExpressionsColumn.FQ_NAME.value, inplace=True)
+    stats.fillna(0, inplace=True)
+    stats = stats.astype(int)
+    stats.reset_index(inplace=True)
+    stats = stats.merge(total_stats, on=CallExpressionsColumn.FQ_NAME.value)
+    logging.info(f'Processed {len(stats)} unique FQ names.')
 
     output_path.parent.mkdir(parents=True, exist_ok=True)
     stats.to_csv(output_path, index=False)
+    logging.info(f'Saving call expressions stats.')
 
 
 if __name__ == '__main__':
