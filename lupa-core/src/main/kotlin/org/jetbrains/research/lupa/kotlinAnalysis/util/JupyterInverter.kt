9437d746691f212e5414efc2a7e5764bd5ee9730
konstantgr <60608301+konstantgr@users.noreply.github.com>
2022-03-16 18:04:05 +0300

    Changing of  imports order

--- lupa-core/src/main/kotlin/org/jetbrains/research/lupa/kotlinAnalysis/util/JupyterInverter.kt
+++ lupa-core/src/main/kotlin/org/jetbrains/research/lupa/kotlinAnalysis/util/JupyterInverter.kt
@@ -1,12 +1,12 @@
 package org.jetbrains.research.lupa
 
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.File
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
-import org.json.JSONArray
-import org.json.JSONObject
 
 class JupyterInverter(input: String, output: String) {
     val inputDir = input
