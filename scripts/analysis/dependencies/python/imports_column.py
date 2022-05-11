b42030c7dfe70358aec9f510f38e7f8827235973
Ilya Vlasov <ilyavlasov2011@gmail.com>
2022-05-11 22:32:39 +0300

    Add wrapper for python import analysis

---
+++ scripts/analysis/dependencies/python/imports_column.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class ImportsColumn(Enum):
+    PROJECT_NAME = 'project_name'
+    IMPORT = 'import'
+    FQ_NAME = 'fq_name'
+    COUNT = 'count'
+    PYTHON_VERSION = 'python_version'
