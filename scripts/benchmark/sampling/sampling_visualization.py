39087705b478a391f6509a35bbc66ec3903dd92a
GirZ0n <ilyavlasov2011@gmail.com>
2022-10-28 13:40:03 +0300

    docs: add help messages for some widgets fix: add chart titles fix: show error message if where is no metrics found

--- scripts/benchmark/sampling/sampling_visualization.py
+++ scripts/benchmark/sampling/sampling_visualization.py
@@ -1,7 +1,7 @@
 import ast
 from enum import Enum, unique
 from pathlib import Path
-from typing import List, Union
+from typing import List, Optional, Union
 
 import numpy as np
 import pandas as pd
@@ -32,7 +32,7 @@ class BinsType(Enum):
 
 
 @st.cache
-def load_data(dataset: Path, language: Language) -> pd.DataFrame:
+def load_data(dataset: Path, language: Language) -> Optional[pd.DataFrame]:
     return read_metrics(dataset, language)
 
 
@@ -44,22 +44,48 @@ def get_bins(key: str) -> Union[int, str, List[float]]:
             'Bins type:',
             options=BinsType.values(),
             index=BinsType.values().index(BinsType.ESTIMATOR.value),
+            help=f"""
+                Possible bins types:
+                * `{BinsType.COUNT.value}` means a number of the bins by which to divide the data.
+                * `{BinsType.ESTIMATOR.value}` means a name of an estimator which automatically determines the
+                  optimal number of the bins.
+                * `{BinsType.EDGES.value}` means an array of edges by which the bins are defined.
+            """,
             key=f'{key}_bins_type',
         )
 
     with right_column:
         if bins_type == BinsType.COUNT.value:
-            return st.number_input('Count:', value=50, min_value=1, key=f'{key}_bins_value')
+            return st.number_input(
+                'Count:',
+                value=50,
+                min_value=1,
+                help='The number of the bins by which to divide the data. The number must be greater than 1.',
+                key=f'{key}_bins_value',
+            )
 
         if bins_type == BinsType.ESTIMATOR.value:
             return st.selectbox(
                 'Name:',
                 options=BinsEstimator.values(),
                 index=BinsEstimator.values().index(BinsEstimator.AUTO.value),
+                help=(
+                    'The name of an estimator. Consistent with [`numpy.histogram_bin_edges`]'
+                    '(https://numpy.org/doc/stable/reference/generated/numpy.histogram_bin_edges.html).'
+                ),
                 key=f'{key}_bins_value',
             )
 
-        return ast.literal_eval(st.text_input('Edges:', value='[0, 10, 20, 30, 40, 50]', key=f'{key}_bins_value'))
+        return ast.literal_eval(
+            st.text_input(
+                'Edges:',
+                value='[0, 10, 20, 30, 40, 50]',
+                help=(
+                    'The array of edges by which the bins are defined. The values must be specified in ascending order.'
+                ),
+                key=f'{key}_bins_value',
+            ),
+        )
 
 
 def get_middle_of_bins(bin_edges: np.ndarray) -> np.ndarray:
@@ -88,6 +114,7 @@ def compare_histograms(
     )
 
     fig.add_bar(x=get_middle_of_bins(bin_edges), y=values, name='Actual')
+    fig.update_layout(xaxis_title='Bin', yaxis_title='Number of projects')
 
     st.plotly_chart(fig, use_container_width=True)
 
@@ -112,8 +139,24 @@ def main():
         st.stop()
 
     row_data = load_data(file_path, Language(language))
+    if row_data is None:
+        st.error('Metrics not found.')
+        st.stop()
+
     data = row_data.dropna(subset=metrics)
-    st.write(f'Number of projects: {len(data)}/{len(row_data)}')
+
+    left_column, right_column = st.columns(2)
+
+    with left_column:
+        st.metric('Total number of projects:', len(row_data), help='The number of projects in the dataset.')
+
+    with right_column:
+        st.metric(
+            'Actual number of projects:',
+            len(data),
+            delta=len(data) - len(row_data),
+            help='The number of projects in which all the specified metrics are present.',
+        )
 
     st.header('Histograms')
 
@@ -124,37 +167,61 @@ def main():
         metric_bins[metric] = get_bins(metric)
         values, bin_edges = np.histogram(data[metric], bins=metric_bins[metric])
 
+        number_of_bins = len(bin_edges) - 1
+        number_of_non_empty_bins = len([value for value in values if value != 0])
+
         left_column, right_column = st.columns(2)
+
         with left_column:
-            st.write(f'Number of bins: {len(bin_edges) - 1}')
+            st.metric('Number of bins:', number_of_bins)
+
         with right_column:
-            st.write(f'Number of non-empty bins: {len([value for value in values if value != 0])}')
+            st.metric(
+                'Number of non-empty bins:',
+                number_of_non_empty_bins,
+                delta=number_of_non_empty_bins - number_of_bins,
+            )
 
         with st.expander('Histogram:'):
-            st.plotly_chart(px.bar(x=get_middle_of_bins(bin_edges), y=values), use_container_width=True)
+            fig = px.bar(x=get_middle_of_bins(bin_edges), y=values)
+            fig.update_layout(xaxis_title='Bin', yaxis_title='Number of projects')
+            st.plotly_chart(fig, use_container_width=True)
 
     st.header('Sampling')
 
     converted_data = convert_to_intervals(data, metric_bins)
-    st.write(f'Number of groups: {len(converted_data.groupby(metrics, observed=True))}')
+    st.metric(
+        'Number of non-empty groups:',
+        len(converted_data.groupby(metrics, observed=True)),
+        help='Number of non-empty groups, after grouping by all metrics.',
+    )
 
     left_column, right_column = st.columns(2)
 
     with left_column:
-        number_of_samples = st.number_input(
-            'Target number of samples:',
+        sample_size = st.number_input(
+            'Sample size:',
             min_value=1,
             value=round(len(converted_data) * 0.2),
+            help='The expected number of projects in the sample.',
         )
 
     with right_column:
-        random_state = st.number_input('Random state:', value=42)
+        random_state = st.number_input('Random state:', value=42, help='Seed for random number generator.')
 
     if st.button('Sample'):
         with st.spinner('Sampling...'):
-            sample = get_stratified_sample(converted_data, metrics, number_of_samples, random_state)
+            sample = get_stratified_sample(converted_data, metrics, sample_size, random_state)
 
-        st.write(f'Actual number of samples: {len(sample)}')
+        st.metric(
+            'Actual sample size:',
+            len(sample),
+            delta=len(sample) - sample_size,
+            help=(
+                'Since it is impossible to collect projects from some small groups with saving the distribution, '
+                'the actual number of projects in the sample may be less than expected. '
+            ),
+        )
 
         for metric in metrics:
             with st.expander(metric):
