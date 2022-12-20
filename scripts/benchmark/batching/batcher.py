8feb18014f597bca523334d2e411c0c943b921fc
GirZ0n <ilyavlasov2011@gmail.com>
2022-12-20 18:30:43 +0300

    feat: add dummy batcher fix: fix argument namings fix: fix typing

--- scripts/benchmark/batching/batcher.py
+++ scripts/benchmark/batching/batcher.py
@@ -2,115 +2,180 @@
 
 import logging
 from abc import ABC, abstractmethod
-from typing import Dict, List, Tuple
+from enum import Enum, unique
+from pathlib import Path
+from typing import Any, Dict, List, Set, Tuple, Type
 
 from binpackp.bins import NumberBin
 from binpackp.fit import BPResult, BinReduction, Fit
-
-from benchmark.batching.config import ConfigField
-from benchmark.metrics_collection.metrics import MetricName
 
 logger = logging.getLogger(__name__)
 
 
 class Batcher(ABC):
+    @classmethod
     @abstractmethod
     def split_into_batches(
-        self,
-        projects: Dict[str, Dict[MetricName, int]],
-        metric_constraints: Dict[MetricName, int],
+        cls,
+        projects: Dict[Path, Dict[str, int]],
+        batch_constraints: Dict[str, int],
         ignore_oversized_projects: bool = True,
         **kwargs,
-    ) -> List[List[str]]:
+    ) -> List[List[Path]]:
         raise NotImplementedError
 
 
+@unique
+class BatcherArgument(Enum):
+    BATCH_SIZE = 'batch_size'
+    MAX_OPEN_BATCHES = 'max_open_batches'
+
+
+class DummyBatcher(Batcher):
+    @classmethod
+    def split_into_batches(
+        cls,
+        projects: Dict[Path, Dict[str, int]],
+        batch_constraints: Dict[str, int],
+        ignore_oversized_projects: bool = True,
+        **kwargs,
+    ) -> List[List[Path]]:
+        if batch_constraints:
+            logger.warning('Batch constraints will be ignored.')
+
+        batch_size = kwargs.get(BatcherArgument.BATCH_SIZE.value, 50)
+        return [list(projects.keys())[i : i + batch_size] for i in range(0, len(projects), batch_size)]
+
+
 class OneDimensionalAnyFitBatcher(Batcher):
+    @classmethod
     @abstractmethod
     def heuristic_split(
-        self,
-        projects: List[Tuple[str, int]],
-        bin_constraint: int,
+        cls,
+        projects: List[Tuple[Path, int]],
+        batch_constraint: int,
         **kwargs,
     ) -> BPResult:
         raise NotImplementedError
 
+    @classmethod
     def split_into_batches(
-        self,
-        projects: Dict[str, Dict[MetricName, int]],
-        metric_constraints: Dict[MetricName, int],
+        cls,
+        projects: Dict[Path, Dict[str, int]],
+        batch_constraints: Dict[str, int],
         ignore_oversized_projects: bool = True,
         **kwargs,
-    ) -> List[List[str]]:
-        if len(metric_constraints) != 1:
-            raise ValueError('Multiple metric constraints are not supported.')
+    ) -> List[List[Path]]:
+        if len(batch_constraints) != 1:
+            raise ValueError('You must pass exactly one batch constraint.')
 
         if not projects:
             return []
 
-        metric_name, metric_constraint = next(iter(metric_constraints.items()))
+        constraint_name, constraint_value = next(iter(batch_constraints.items()))
 
         projects_for_batching = [
-            (project, metrics[metric_name])
+            (project, metrics[constraint_name])
             for project, metrics in projects.items()
-            if metrics.get(metric_name) is not None
+            if metrics.get(constraint_name) is not None
         ]
 
         if len(projects) != len(projects_for_batching):
-            logger.warning(f"Projects without the '{metric_name.value}' metric will be skipped.")
+            logger.warning(
+                f'{len(projects) - len(projects_for_batching)} projects without '
+                f"the '{constraint_name}' metric will be skipped.",
+            )
 
-        oversized_bins = [
-            [project] for project, metric_value in projects_for_batching if metric_value > metric_constraint
+        oversized_batches = [
+            [project] for project, metric_value in projects_for_batching if metric_value > constraint_value
         ]
 
         projects_for_batching = [
             (project, metric_value)
             for project, metric_value in projects_for_batching
-            if metric_value <= metric_constraint
+            if metric_value <= constraint_value
         ]
 
-        if oversized_bins:
+        if oversized_batches:
             if ignore_oversized_projects:
-                logger.warning(f'{len(oversized_bins)} oversized projects will be ignored.')
-                oversized_bins.clear()
+                logger.warning(f'{len(oversized_batches)} oversized projects will be ignored.')
+                oversized_batches.clear()
             else:
-                logger.warning(f'{len(oversized_bins)} oversized projects will be placed in separate bins.')
+                logger.warning(f'{len(oversized_batches)} oversized projects will be placed in separate batches.')
 
-        heuristic_result = self.heuristic_split(projects_for_batching, metric_constraint, **kwargs)
-        return [[project for project, _ in result_bin] for result_bin in heuristic_result.bins] + oversized_bins
+        heuristic_result = cls.heuristic_split(projects_for_batching, constraint_value, **kwargs)
+        return [[project for project, _ in result_bin] for result_bin in heuristic_result.bins] + oversized_batches
 
 
 class NamedNumberBin(NumberBin):
     @classmethod
-    def size(cls, item: Tuple[str, int]):
+    def size(cls, item: Tuple[Any, int]):
         _, value = item
         return super().size(value)
 
 
 class OneDimensionalFirstFitDecreasingBatcher(OneDimensionalAnyFitBatcher):
-    def heuristic_split(self, projects: List[Tuple[str, int]], bin_constraint: int, **kwargs) -> BPResult:
-        return Fit.ffd(NamedNumberBin, bin_constraint, projects)
+    @classmethod
+    def heuristic_split(cls, projects: List[Tuple[Path, int]], batch_constraint: int, **kwargs) -> BPResult:
+        return Fit.ffd(NamedNumberBin, batch_constraint, projects)
 
 
 class OneDimensionalBestFitDecreasingBatcher(OneDimensionalAnyFitBatcher):
-    def heuristic_split(self, projects: List[Tuple[str, int]], bin_constraint: int, **kwargs) -> BPResult:
-        return Fit.bfd(NamedNumberBin, bin_constraint, projects)
+    @classmethod
+    def heuristic_split(cls, projects: List[Tuple[Path, int]], batch_constraint: int, **kwargs) -> BPResult:
+        return Fit.bfd(NamedNumberBin, batch_constraint, projects)
 
 
 class OneDimensionalWorstFitDecreasingBatcher(OneDimensionalAnyFitBatcher):
-    def heuristic_split(self, projects: List[Tuple[str, int]], bin_constraint: int, **kwargs) -> BPResult:
-        return Fit.wfd(NamedNumberBin, bin_constraint, projects)
+    @classmethod
+    def heuristic_split(cls, projects: List[Tuple[Path, int]], batch_constraint: int, **kwargs) -> BPResult:
+        return Fit.wfd(NamedNumberBin, batch_constraint, projects)
 
 
 class OneDimensionalNextFitDecreasingBatcher(OneDimensionalAnyFitBatcher):
-    def heuristic_split(self, projects: List[Tuple[str, int]], bin_constraint: int, **kwargs) -> BPResult:
-        max_open_bins = kwargs.get(ConfigField.MAX_OPEN_BINS.value, 1)
-
+    @classmethod
+    def heuristic_split(cls, projects: List[Tuple[Path, int]], batch_constraint: int, **kwargs) -> BPResult:
         return Fit.fit(
             NamedNumberBin,
-            bin_constraint,
+            batch_constraint,
             projects,
             sort=True,
-            max_open_bins=max_open_bins,
+            max_open_bins=kwargs.get(BatcherArgument.MAX_OPEN_BATCHES.value, 1),
             bin_reduction=BinReduction.first,
         )
+
+
+@unique
+class BatcherName(Enum):
+    DUMMY_BATCHER = 'dummy_batcher'
+    ONE_DIMENSIONAL_FIRST_FIT_DECREASING = '1d_first_fit_decreasing'
+    ONE_DIMENSIONAL_BEST_FIT_DECREASING = '1d_best_fit_decreasing'
+    ONE_DIMENSIONAL_WORST_FIT_DECREASING = '1d_worst_fit_decreasing'
+    ONE_DIMENSIONAL_NEXT_FIT_DECREASING = '1d_next_fit_decreasing'
+
+    def execute(
+        self,
+        projects: Dict[Path, Dict[str, int]],
+        batch_constraints: Dict[str, int],
+        ignore_oversized_batches: bool = True,
+        **kwargs,
+    ) -> List[List[Path]]:
+        return BATCHER_NAME_TO_CLASS[self].split_into_batches(
+            projects,
+            batch_constraints,
+            ignore_oversized_batches,
+            **kwargs,
+        )
+
+    @classmethod
+    def values(cls) -> Set[str]:
+        return {metric.value for metric in cls}
+
+
+BATCHER_NAME_TO_CLASS: Dict[BatcherName, Type[Batcher]] = {
+    BatcherName.DUMMY_BATCHER: DummyBatcher,
+    BatcherName.ONE_DIMENSIONAL_FIRST_FIT_DECREASING: OneDimensionalFirstFitDecreasingBatcher,
+    BatcherName.ONE_DIMENSIONAL_BEST_FIT_DECREASING: OneDimensionalBestFitDecreasingBatcher,
+    BatcherName.ONE_DIMENSIONAL_WORST_FIT_DECREASING: OneDimensionalWorstFitDecreasingBatcher,
+    BatcherName.ONE_DIMENSIONAL_NEXT_FIT_DECREASING: OneDimensionalNextFitDecreasingBatcher,
+}
--- scripts/test/benchmark/batching/test_batcher.py
+++ scripts/test/benchmark/batching/test_batcher.py
@@ -1,189 +1,286 @@
+from pathlib import Path
 from typing import Dict, List, Type
 
 import pytest
 
 from benchmark.batching.batcher import (
+    DummyBatcher,
     OneDimensionalAnyFitBatcher,
     OneDimensionalBestFitDecreasingBatcher,
     OneDimensionalFirstFitDecreasingBatcher,
     OneDimensionalNextFitDecreasingBatcher,
     OneDimensionalWorstFitDecreasingBatcher,
+    BatcherArgument,
 )
 from benchmark.batching.config import ConfigField
 from benchmark.metrics_collection.metrics import MetricName
 
 
 COMMON_PROJECTS = {
-    'project0': {MetricName.FILE_SIZE: 1, MetricName.NUMBER_OF_FILES: 9},
-    'project1': {MetricName.FILE_SIZE: 8, MetricName.NUMBER_OF_FILES: 5},
-    'project2': {MetricName.FILE_SIZE: 3, MetricName.NUMBER_OF_FILES: 6},
-    'project3': {MetricName.FILE_SIZE: 9, MetricName.NUMBER_OF_FILES: 8},
-    'project4': {MetricName.FILE_SIZE: 2, MetricName.NUMBER_OF_FILES: 7},
-    'project5': {MetricName.FILE_SIZE: 9, MetricName.NUMBER_OF_FILES: 3},
-    'project6': {MetricName.FILE_SIZE: 7, MetricName.NUMBER_OF_FILES: 1},
-    'oversized_project': {MetricName.FILE_SIZE: 5, MetricName.NUMBER_OF_FILES: 20},
-    'project_without_number_of_files': {MetricName.FILE_SIZE: 6},
+    Path('project0'): {MetricName.FILE_SIZE.value: 1, MetricName.NUMBER_OF_FILES.value: 9},
+    Path('project1'): {MetricName.FILE_SIZE.value: 8, MetricName.NUMBER_OF_FILES.value: 5},
+    Path('project2'): {MetricName.FILE_SIZE.value: 3, MetricName.NUMBER_OF_FILES.value: 6},
+    Path('project3'): {MetricName.FILE_SIZE.value: 9, MetricName.NUMBER_OF_FILES.value: 8},
+    Path('project4'): {MetricName.FILE_SIZE.value: 2, MetricName.NUMBER_OF_FILES.value: 7},
+    Path('project5'): {MetricName.FILE_SIZE.value: 9, MetricName.NUMBER_OF_FILES.value: 3},
+    Path('project6'): {MetricName.FILE_SIZE.value: 7, MetricName.NUMBER_OF_FILES.value: 1},
+    Path('oversized_project'): {MetricName.FILE_SIZE.value: 5, MetricName.NUMBER_OF_FILES.value: 20},
+    Path('project_without_number_of_files'): {MetricName.FILE_SIZE.value: 6},
 }
 
-EMPTY_BIN_CONSTRAINTS = {}
-SINGLE_BIN_CONSTRAINTS = {MetricName.NUMBER_OF_FILES: 10}
-SEVERAL_BIN_CONSTRAINTS = {MetricName.NUMBER_OF_FILES: 10, MetricName.FILE_SIZE: 15}
+EMPTY_BATCH_CONSTRAINTS = {}
+SINGLE_BATCH_CONSTRAINTS = {MetricName.NUMBER_OF_FILES.value: 10}
+SEVERAL_BATCH_CONSTRAINTS = {MetricName.NUMBER_OF_FILES.value: 10, MetricName.FILE_SIZE.value: 15}
 
 EMPTY_KWARGS = {}
 
 
 ONE_DIMENSIONAL_ANY_FIT_BATCHER_TEST_DATA = [
     # Empty project list
-    (OneDimensionalFirstFitDecreasingBatcher, {}, SINGLE_BIN_CONSTRAINTS, EMPTY_KWARGS, []),
-    (OneDimensionalBestFitDecreasingBatcher, {}, SINGLE_BIN_CONSTRAINTS, EMPTY_KWARGS, []),
-    (OneDimensionalWorstFitDecreasingBatcher, {}, SINGLE_BIN_CONSTRAINTS, EMPTY_KWARGS, []),
-    (OneDimensionalNextFitDecreasingBatcher, {}, SINGLE_BIN_CONSTRAINTS, EMPTY_KWARGS, []),
-    (OneDimensionalNextFitDecreasingBatcher, {}, SINGLE_BIN_CONSTRAINTS, {ConfigField.MAX_OPEN_BINS.value: 3}, []),
+    (OneDimensionalFirstFitDecreasingBatcher, {}, SINGLE_BATCH_CONSTRAINTS, EMPTY_KWARGS, []),
+    (OneDimensionalBestFitDecreasingBatcher, {}, SINGLE_BATCH_CONSTRAINTS, EMPTY_KWARGS, []),
+    (OneDimensionalWorstFitDecreasingBatcher, {}, SINGLE_BATCH_CONSTRAINTS, EMPTY_KWARGS, []),
+    (OneDimensionalNextFitDecreasingBatcher, {}, SINGLE_BATCH_CONSTRAINTS, EMPTY_KWARGS, []),
+    (
+        OneDimensionalNextFitDecreasingBatcher,
+        {},
+        SINGLE_BATCH_CONSTRAINTS,
+        {BatcherArgument.MAX_OPEN_BATCHES.value: 3},
+        [],
+    ),
     # Non-empty project list
     (
         OneDimensionalFirstFitDecreasingBatcher,
         COMMON_PROJECTS,
-        SINGLE_BIN_CONSTRAINTS,
+        SINGLE_BATCH_CONSTRAINTS,
         EMPTY_KWARGS,
-        [['project4', 'project5'], ['project0', 'project6'], ['project3'], ['project2'], ['project1']],
+        [
+            [Path('project4'), Path('project5')],
+            [Path('project0'), Path('project6')],
+            [Path('project3')],
+            [Path('project2')],
+            [Path('project1')],
+        ],
     ),
     (
         OneDimensionalBestFitDecreasingBatcher,
         COMMON_PROJECTS,
-        SINGLE_BIN_CONSTRAINTS,
+        SINGLE_BATCH_CONSTRAINTS,
         EMPTY_KWARGS,
-        [['project4', 'project5'], ['project0', 'project6'], ['project3'], ['project2'], ['project1']],
+        [
+            [Path('project4'), Path('project5')],
+            [Path('project0'), Path('project6')],
+            [Path('project3')],
+            [Path('project2')],
+            [Path('project1')],
+        ],
     ),
     (
         OneDimensionalWorstFitDecreasingBatcher,
         COMMON_PROJECTS,
-        SINGLE_BIN_CONSTRAINTS,
+        SINGLE_BATCH_CONSTRAINTS,
         EMPTY_KWARGS,
-        [['project2', 'project6'], ['project4'], ['project1', 'project5'], ['project3'], ['project0']],
+        [
+            [Path('project2'), Path('project6')],
+            [Path('project4')],
+            [Path('project1'), Path('project5')],
+            [Path('project3')],
+            [Path('project0')],
+        ],
     ),
     (
         OneDimensionalNextFitDecreasingBatcher,
         COMMON_PROJECTS,
-        SINGLE_BIN_CONSTRAINTS,
+        SINGLE_BATCH_CONSTRAINTS,
         EMPTY_KWARGS,
-        [['project0'], ['project3'], ['project4'], ['project2'], ['project1', 'project5', 'project6']],
+        [
+            [Path('project0')],
+            [Path('project3')],
+            [Path('project4')],
+            [Path('project2')],
+            [Path('project1'), Path('project5'), Path('project6')],
+        ],
     ),
     (
         OneDimensionalNextFitDecreasingBatcher,
         COMMON_PROJECTS,
-        SINGLE_BIN_CONSTRAINTS,
-        {ConfigField.MAX_OPEN_BINS.value: 3},
-        [['project0'], ['project3'], ['project4', 'project5'], ['project2', 'project6'], ['project1']],
+        SINGLE_BATCH_CONSTRAINTS,
+        {BatcherArgument.MAX_OPEN_BATCHES.value: 3},
+        [
+            [Path('project0')],
+            [Path('project3')],
+            [Path('project4'), Path('project5')],
+            [Path('project2'), Path('project6')],
+            [Path('project1')],
+        ],
     ),
     # Non-empty project list without ignoring oversized projects
     (
         OneDimensionalFirstFitDecreasingBatcher,
         COMMON_PROJECTS,
-        SINGLE_BIN_CONSTRAINTS,
+        SINGLE_BATCH_CONSTRAINTS,
         {ConfigField.IGNORE_OVERSIZED_PROJECTS.value: False},
         [
-            ['project4', 'project5'],
-            ['project0', 'project6'],
-            ['project3'],
-            ['project2'],
-            ['project1'],
-            ['oversized_project'],
+            [Path('project4'), Path('project5')],
+            [Path('project0'), Path('project6')],
+            [Path('project3')],
+            [Path('project2')],
+            [Path('project1')],
+            [Path('oversized_project')],
         ],
     ),
     (
         OneDimensionalBestFitDecreasingBatcher,
         COMMON_PROJECTS,
-        SINGLE_BIN_CONSTRAINTS,
+        SINGLE_BATCH_CONSTRAINTS,
         {ConfigField.IGNORE_OVERSIZED_PROJECTS.value: False},
         [
-            ['project4', 'project5'],
-            ['project0', 'project6'],
-            ['project3'],
-            ['project2'],
-            ['project1'],
-            ['oversized_project'],
+            [Path('project4'), Path('project5')],
+            [Path('project0'), Path('project6')],
+            [Path('project3')],
+            [Path('project2')],
+            [Path('project1')],
+            [Path('oversized_project')],
         ],
     ),
     (
         OneDimensionalWorstFitDecreasingBatcher,
         COMMON_PROJECTS,
-        SINGLE_BIN_CONSTRAINTS,
+        SINGLE_BATCH_CONSTRAINTS,
         {ConfigField.IGNORE_OVERSIZED_PROJECTS.value: False},
         [
-            ['project2', 'project6'],
-            ['project4'],
-            ['project1', 'project5'],
-            ['project3'],
-            ['project0'],
-            ['oversized_project'],
+            [Path('project2'), Path('project6')],
+            [Path('project4')],
+            [Path('project1'), Path('project5')],
+            [Path('project3')],
+            [Path('project0')],
+            [Path('oversized_project')],
         ],
     ),
     (
         OneDimensionalNextFitDecreasingBatcher,
         COMMON_PROJECTS,
-        SINGLE_BIN_CONSTRAINTS,
+        SINGLE_BATCH_CONSTRAINTS,
         {ConfigField.IGNORE_OVERSIZED_PROJECTS.value: False},
         [
-            ['project0'],
-            ['project3'],
-            ['project4'],
-            ['project2'],
-            ['project1', 'project5', 'project6'],
-            ['oversized_project'],
+            [Path('project0')],
+            [Path('project3')],
+            [Path('project4')],
+            [Path('project2')],
+            [Path('project1'), Path('project5'), Path('project6')],
+            [Path('oversized_project')],
         ],
     ),
     (
         OneDimensionalNextFitDecreasingBatcher,
         COMMON_PROJECTS,
-        SINGLE_BIN_CONSTRAINTS,
-        {ConfigField.IGNORE_OVERSIZED_PROJECTS.value: False, ConfigField.MAX_OPEN_BINS.value: 3},
+        SINGLE_BATCH_CONSTRAINTS,
+        {ConfigField.IGNORE_OVERSIZED_PROJECTS.value: False, BatcherArgument.MAX_OPEN_BATCHES.value: 3},
         [
-            ['project0'],
-            ['project3'],
-            ['project4', 'project5'],
-            ['project2', 'project6'],
-            ['project1'],
-            ['oversized_project'],
+            [Path('project0')],
+            [Path('project3')],
+            [Path('project4'), Path('project5')],
+            [Path('project2'), Path('project6')],
+            [Path('project1')],
+            [Path('oversized_project')],
         ],
     ),
 ]
 
 
 @pytest.mark.parametrize(
-    ('batcher', 'projects', 'metric_constraints', 'kwargs', 'expected_bins'),
+    ('batcher', 'projects', 'batches_constraints', 'kwargs', 'expected_batches'),
     ONE_DIMENSIONAL_ANY_FIT_BATCHER_TEST_DATA,
 )
 def test_one_dimensional_any_fit_batcher(
     batcher: Type[OneDimensionalAnyFitBatcher],
-    projects: Dict[str, Dict[MetricName, int]],
-    metric_constraints: Dict[MetricName, int],
+    projects: Dict[Path, Dict[str, int]],
+    batches_constraints: Dict[str, int],
     kwargs: Dict,
-    expected_bins: List[List[str]],
+    expected_batches: List[List[str]],
 ) -> None:
-    assert batcher().split_into_batches(projects, metric_constraints, **kwargs) == expected_bins
+    assert batcher().split_into_batches(projects, batches_constraints, **kwargs) == expected_batches
 
 
 ONE_DIMENSIONAL_ANY_FIT_BATCHER_INVALID_METRIC_CONSTRAINTS_TEST_DATA = [
-    # Empty bin constraints
-    (OneDimensionalFirstFitDecreasingBatcher, EMPTY_BIN_CONSTRAINTS),
-    (OneDimensionalBestFitDecreasingBatcher, EMPTY_BIN_CONSTRAINTS),
-    (OneDimensionalWorstFitDecreasingBatcher, EMPTY_BIN_CONSTRAINTS),
-    (OneDimensionalNextFitDecreasingBatcher, EMPTY_BIN_CONSTRAINTS),
-    # Several bin constraints
-    (OneDimensionalFirstFitDecreasingBatcher, SEVERAL_BIN_CONSTRAINTS),
-    (OneDimensionalBestFitDecreasingBatcher, SEVERAL_BIN_CONSTRAINTS),
-    (OneDimensionalWorstFitDecreasingBatcher, SEVERAL_BIN_CONSTRAINTS),
-    (OneDimensionalNextFitDecreasingBatcher, SEVERAL_BIN_CONSTRAINTS),
+    # Empty batch constraints
+    (OneDimensionalFirstFitDecreasingBatcher, EMPTY_BATCH_CONSTRAINTS),
+    (OneDimensionalBestFitDecreasingBatcher, EMPTY_BATCH_CONSTRAINTS),
+    (OneDimensionalWorstFitDecreasingBatcher, EMPTY_BATCH_CONSTRAINTS),
+    (OneDimensionalNextFitDecreasingBatcher, EMPTY_BATCH_CONSTRAINTS),
+    # Several batch constraints
+    (OneDimensionalFirstFitDecreasingBatcher, SEVERAL_BATCH_CONSTRAINTS),
+    (OneDimensionalBestFitDecreasingBatcher, SEVERAL_BATCH_CONSTRAINTS),
+    (OneDimensionalWorstFitDecreasingBatcher, SEVERAL_BATCH_CONSTRAINTS),
+    (OneDimensionalNextFitDecreasingBatcher, SEVERAL_BATCH_CONSTRAINTS),
 ]
 
 
 @pytest.mark.parametrize(
-    ('batcher', 'metric_constraints'),
+    ('batcher', 'batch_constraints'),
     ONE_DIMENSIONAL_ANY_FIT_BATCHER_INVALID_METRIC_CONSTRAINTS_TEST_DATA,
 )
-def test_one_dimensional_any_fit_batcher_invalid_metric_constraints(
+def test_one_dimensional_any_fit_batcher_invalid_batch_constraints(
     batcher: Type[OneDimensionalAnyFitBatcher],
-    metric_constraints: Dict[MetricName, int],
+    batch_constraints: Dict[str, int],
 ) -> None:
     with pytest.raises(ValueError):
         batcher().split_into_batches(COMMON_PROJECTS, {})
+
+
+DUMMY_BATCHER_TEST_DATA = [
+    ([], EMPTY_KWARGS, []),
+    ([], {BatcherArgument.BATCH_SIZE.value: 4}, []),
+    (
+        COMMON_PROJECTS,
+        EMPTY_KWARGS,
+        [
+            [
+                Path('project0'),
+                Path('project1'),
+                Path('project2'),
+                Path('project3'),
+                Path('project4'),
+                Path('project5'),
+                Path('project6'),
+                Path('oversized_project'),
+                Path('project_without_number_of_files'),
+            ]
+        ],
+    ),
+    (
+        COMMON_PROJECTS,
+        {BatcherArgument.BATCH_SIZE.value: 4},
+        [
+            [Path('project0'), Path('project1'), Path('project2'), Path('project3')],
+            [Path('project4'), Path('project5'), Path('project6'), Path('oversized_project')],
+            [Path('project_without_number_of_files')],
+        ],
+    ),
+    (
+        COMMON_PROJECTS,
+        {BatcherArgument.BATCH_SIZE.value: 10},
+        [
+            [
+                Path('project0'),
+                Path('project1'),
+                Path('project2'),
+                Path('project3'),
+                Path('project4'),
+                Path('project5'),
+                Path('project6'),
+                Path('oversized_project'),
+                Path('project_without_number_of_files'),
+            ]
+        ],
+    ),
+]
+
+
+@pytest.mark.parametrize(('projects', 'kwargs', 'expected_batches'), DUMMY_BATCHER_TEST_DATA)
+def test_dummy_batcher(
+    projects: Dict[Path, Dict[str, int]],
+    kwargs: Dict,
+    expected_batches: List[List[str]],
+) -> None:
+    assert DummyBatcher().split_into_batches(projects, {}, **kwargs) == expected_batches
