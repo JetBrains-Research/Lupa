c5325010f08160ade3b816ef2de12ea1798c41e5
GirZ0n <ilyavlasov2011@gmail.com>
2022-12-20 16:31:42 +0300

    feat: add a batching config

---
+++ scripts/benchmark/batching/config.py
@@ -0,0 +1,61 @@
+import json
+from enum import Enum, unique
+
+from benchmark.batching.batcher import BatcherArgument, BatcherName
+from benchmark.metrics_collection.metrics import MetricName
+from utils.config_utils import check_config
+from utils.language import Language
+
+
+@unique
+class ConfigField(Enum):
+    BATCH_CONSTRAINTS = 'batch_constraints'
+
+    LANGUAGE = 'language'
+    IGNORE_OVERSIZED_PROJECTS = 'ignore_oversized_projects'
+    METRIC = 'metric'
+
+    BATCHER_CONFIG = 'batcher_config'
+    NAME = 'name'
+
+
+BATCHING_SCHEMA = {
+    ConfigField.LANGUAGE.value: {
+        'type': 'string',
+        'required': False,
+        'allowed': Language.values(),
+        'dependencies': [ConfigField.METRIC.value, ConfigField.BATCH_CONSTRAINTS.value],
+    },
+    ConfigField.METRIC.value: {
+        'type': 'string',
+        'required': False,
+        'allowed': MetricName.values(),
+        'dependencies': [ConfigField.LANGUAGE.value, ConfigField.BATCH_CONSTRAINTS.value],
+    },
+    ConfigField.BATCH_CONSTRAINTS.value: {
+        'type': 'dict',
+        'required': False,
+        'keysrules': {'type': 'string', 'allowed': MetricName.values()},
+        'valuesrules': {'type': 'integer'},
+        'dependencies': [ConfigField.LANGUAGE.value, ConfigField.METRIC.value],
+    },
+    ConfigField.IGNORE_OVERSIZED_PROJECTS.value: {'type': 'boolean', 'required': False},
+    ConfigField.BATCHER_CONFIG.value: {
+        'type': 'dict',
+        'schema': {
+            ConfigField.NAME.value: {'type': 'string', 'required': True, 'allowed': BatcherName.values()},
+            BatcherArgument.BATCH_SIZE.value: {
+                'type': 'integer',
+                'required': False,
+                'min': 1,
+                'dependencies': {ConfigField.NAME.value: [BatcherName.DUMMY_BATCHER.value]},
+            },
+            BatcherArgument.MAX_OPEN_BATCHES.value: {
+                'type': 'integer',
+                'required': False,
+                'min': 1,
+                'dependencies': {ConfigField.NAME.value: [BatcherName.ONE_DIMENSIONAL_NEXT_FIT_DECREASING.value]},
+            },
+        },
+    },
+}
